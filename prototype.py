e3c4814694b083b7104f1af3a6f1ae3f9cda30e1
Bennett <bcc2761@g.rit.edu>
2023-10-09 15:36:11 -0400

    initialization of files and sample code

---
+++ prototype.py
@@ -0,0 +1,14 @@
+import click
+
+@click.command()
+@click.argument('name')
+@click.option('--uppercase', '-u', is_falg=True, help = 'Print name in uppercase.')
+def hello():
+    //click.echo(f"Hello, {name}!")
+    greeting = f"Hello, {name}!"
+    if uppercase:
+        greeting = greeting.upper()
+    click.echo(greeting)
+
+if __name__ == '__main__':
+    hello()
