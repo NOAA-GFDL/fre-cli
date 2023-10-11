29e8637916e9e028ae52e88282102271c4fdaaf2
Bennett Chang <Bennett.Chang@ldt-4742405.gfdl.noaa.gov>
2023-10-11 16:58:28 -0400

    added more to playtest file

--- playtest.py
+++ playtest.py
@@ -1,5 +1,11 @@
 import click
 
+# click group allows for multiple functions to be called via same script
+@click.group()
+def cli():
+    pass
+
+# first function, hello
 @click.command()
 @click.argument('name')
 @click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
@@ -10,5 +16,20 @@ def hello(name, uppercase):
         greeting = greeting.upper()
     click.echo(greeting)
 
+# second function, goodbye
+@click.command()
+@click.argument('name')
+@click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
+def goodbye(name, uppercase):
+    #click.echo(f"Hello, {name}!")
+    farewell = f"Goodbye, {name}!"
+    if uppercase:
+        farewell = farewell.upper()
+    click.echo(farewell)
+
+# add your functions to the click group
+cli.add_command(hello)
+cli.add_command(goodbye)
+
 if __name__ == '__main__':
-    hello()
+    cli()
