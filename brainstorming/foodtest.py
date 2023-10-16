95784f74a3c7000643ddb6e65a8853141cdc9c70
Bennett Chang <bcc2761@g.rit.edu>
2023-10-16 16:05:30 -0400

    more changes added money

--- brainstorming/foodtest.py
+++ brainstorming/foodtest.py
@@ -17,36 +17,43 @@ def food():
 @click.option('--mushroom', '-m', is_flag=True, help = 'Add mushrooms to burger.')
 def burger(name_of_order, impossible, cheese, ketchup, lettuce, tomato, pickle, mushroom):
     
-    order = f"Your order is named {name_of_order}."
     selected_options =[]
+    total = 5.00
+    order = f"Your order is named {name_of_order}."
 
     if impossible:
         click.echo('You have substituted for impossible meat.')
         selected_options.append('impossible meat')
+        total += 1.00
     if cheese:
         click.echo('You have added cheese.')
         selected_options.append('cheese')
+        total += 0.50
     if ketchup:
         click.echo('You have added ketchup.')
         selected_options.append('ketchup')
     if lettuce:
         click.echo('You have added lettuce.')
         selected_options.append('lettuce')
+        total += 0.50
     if tomato:
         click.echo('You have added tomato.')
         selected_options.append('tomato')
+        total += 0.50
     if pickle:
         click.echo('You have added pickles.')
         selected_options.append('pickles')
+        total += 0.25
     if mushroom:
         click.echo('You have added mushrooms.')
         selected_options.append('pickles')
+        total += 0.25
     
     options_text = ', '.join(selected_options)
     if options_text:
-        click.echo(f"{order} Your burger includes {options_text}.")
+        click.echo(f"{order} Your burger includes {options_text}. The total is ${total:.2f}")
     else:
-        click.echo(f"{order} No added ingredients/toppings.")
+        click.echo(f"{order} No added ingredients/toppings. The total is ${total:.2f}")
 
 @food.command()
 @click.argument('name_of_order')
@@ -56,40 +63,33 @@ def burger(name_of_order, impossible, cheese, ketchup, lettuce, tomato, pickle, 
 @click.option('--relish', '-r', is_flag=True, help = 'Add relish to hotdog.')
 def hotdog(name_of_order, cheese, mustard, ketchup, relish):
     
-    order = f"Your order is named {name_of_order}."
     selected_options =[]
+    total = 2.50
+    order = f"Your order is named {name_of_order}."
 
     if cheese:
         click.echo('You have added cheese sauce.')
         selected_options.append('cheese sauce')
+        total += 0.15
     if mustard:
         click.echo('You have added mustard.')
         selected_options.append('mustard')
+        total += 0.15
     if ketchup:
         click.echo('You have added ketchup.')
         selected_options.append('ketchup')
+        total += 0.15
     if relish:
         click.echo('You have added relish.')
         selected_options.append('relish')    
-
+        total += 0.15
     options_text = ', '.join(selected_options)
     if options_text:
-        click.echo(f"{order} Your burger includes {options_text}.")
+        click.echo(f"{order} Your hot dog includes {options_text}. The total is ${total:.2f}")
     else:
-        click.echo(f"{order} No added ingredients/toppings.")
+        click.echo(f"{order} No added ingredients/toppings. The total is ${total:.2f}")
 
-@food.command()
-def sandwich():
-    pass
-
-@food.command()
-def pasta():
-    pass
-
-@food.command()
-def pastry():
-    pass
-
+"""
 # first function, hello
 @click.command()
 @click.argument('name')
@@ -100,6 +100,7 @@ def hello(name, uppercase):
     if uppercase:
         greeting = greeting.upper()
     click.echo(greeting)
+"""
 
 if __name__ == '__main__':
     food()
