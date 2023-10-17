6a2e7df9dadb48b80e5fb3372786793433203beb
Bennett <bcc2761@g.rit.edu>
2023-10-17 13:42:24 -0400

    want to incorporate classes

--- brainstorming/foodtest2.py
+++ brainstorming/foodtest2.py
@@ -2,7 +2,6 @@
 This program is written to familiarize with click's .confirm() tools and seek to do something similar to foodtest.py
 """
 import click
-import math
 
 total = 0.00
 
@@ -12,7 +11,8 @@ def get_burger_options():
     while True:
         selected_options = []
         total += 5.00
-        
+        subtotal = 5.00
+
         impossible = click.confirm('Do you want to substitute beef patty for impossible patty?')
         cheese = click.confirm('Do you want to add cheese to the burger?')
         ketchup = click.confirm('Do you want to add ketchup to the burger?')
@@ -25,10 +25,12 @@ def get_burger_options():
             click.echo('You have substituted for impossible meat.')
             selected_options.append('impossible meat')
             total += 1.00
+            subtotal += 1.00
         if cheese:
             click.echo('You have added cheese.')
             selected_options.append('cheese')
             total += 0.50
+            subtotal += 0.50
         if ketchup:
             click.echo('You have added ketchup.')
             selected_options.append('ketchup')
@@ -36,36 +38,43 @@ def get_burger_options():
             click.echo('You have added lettuce.')
             selected_options.append('lettuce')
             total += 0.50
+            subtotal += 0.50
         if tomato:
             click.echo('You have added tomato.')
             selected_options.append('tomato')
             total += 0.50
+            subtotal += 0.50
         if pickle:
             click.echo('You have added pickles.')
             selected_options.append('pickles')
             total += 0.25
+            subtotal += 0.25
         if mushroom:
             click.echo('You have added mushrooms.')
             selected_options.append('mushrooms')
             total += 0.25
+            subtotal += 0.25
 
+        
 
         # Want to make a list to add orders and have a cumulative print summary at the end 
 
         options_text = ', '.join(selected_options)
         if options_text:
-            click.echo(f"Your burger includes {options_text}. The total is ${total:.2f}")
+            click.echo(f"Your burger includes {options_text}. The subtotal is ${subtotal:.2f}")
         else:
-            click.echo("No added ingredients/toppings.")
+            click.echo(f"No added ingredients/toppings. The subtotal is ${subtotal:.2f}")
         
         more_food = click.confirm('Do you want to order more food?')
         if not more_food:
+            click.echo(f"Your order total is ${total:.2f}")
             break
+
 
 @click.command()
 @click.argument('name_of_order')
 def burger(name_of_order):
-    order = f"Your order is named {name_of_order}."
+    order = f"Your order is for {name_of_order}."
     click.echo(order)
     get_burger_options()
 
