e850515773d4fe8e397cdf0f6e51e703d4e13a05
Bennett Chang <bcc2761@g.rit.edu>
2023-10-18 13:00:12 -0400

    removed comments

--- fre.py
+++ fre.py
@@ -40,7 +40,7 @@ def checkout(uppercase):
     if uppercase:
         statement = statement.upper()
     click.echo(statement)
-    
+
 @fremake.command()
 def compile():
     click.echo("execute fre make compile script")
@@ -52,33 +52,7 @@ def container():
 @fremake.command()
 def list():
     click.echo("execute fre make list script")
-
-
-# # first function, hello
-# @click.command()
-# @click.argument('name')
-# @click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
-# def hello(name, uppercase):
-#     #click.echo(f"Hello, {name}!")
-#     greeting = f"Hello, {name}!"
-#     if uppercase:
-#         greeting = greeting.upper()
-#     click.echo(greeting)
-# 
-# # second function, goodbye
-# @click.command()
-# @click.argument('name')
-# @click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
-# def goodbye(name, uppercase):
-#     #click.echo(f"Hello, {name}!")
-#     farewell = f"Goodbye, {name}!"
-#     if uppercase:
-#         farewell = farewell.upper()
-#     click.echo(farewell)
-# 
-# # add your functions to the click group
-# cli.add_command(hello)
-# cli.add_command(goodbye)
-
+    
+    
 if __name__ == '__main__':
     fre()
