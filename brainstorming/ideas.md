70ed631810ab416a684a62bc854a3ed0a86c073c
Bennett Chang <bcc2761@g.rit.edu>
2023-10-18 12:59:16 -0400

    progress made on actual fre prototype per Chris's suggestions

--- brainstorming/ideas.md
+++ brainstorming/ideas.md
@@ -20,5 +20,11 @@
 * do we want to use flags (`click.option()`), confirmations (`click.confirm()`), or a mix of both to allow users to run what they want, how they want?
     - this means that users can either use certain flags (i.e `--execute`), which will be included and explained in the `--help` feature, or they will just be prompted for what features they want and can decide if they want it with [y/N]
 
+## Updates/Things to Consider
+* use of classes, arguments (necessary) vs. flags (optional)
+    - arguments can be used for specific cases; i.e need to parse specific file
+* per Chris's input, need nested groups to allow subcommands to be executed with specific flags
+    - will probably implement an `execute_all` command within a group like `fre make` if user wants to execute every feature at once, i.e. `checkout`, `container`, `list`, and `compile` for `fre make`
+
 ## Potential Additional Uses for Click
 * program using BeautifulSoup to scrape GFDL pages for immediate tutorial guidance after prompting for GFDL login
---
+++ fre.py
@@ -0,0 +1,84 @@
+import click
+
+"""
+click group allows for multiple functions to be called via same script
+"""
+@click.group()
+def fre():
+    pass
+
+"""
+this is a nested group within the {fre} group that allows commands to be processed
+"""
+@fre.group('list')
+def frelist():
+    pass
+
+@fre.group('make')
+def fremake():
+    pass
+
+@fre.group('run')
+def frerun():
+    pass
+
+@fre.group('postprocess')
+def frepostprocess():
+    pass
+
+@fre.group('check')
+def frecheck():
+    pass
+
+"""
+{fremake} subcommands to be processed
+"""
+@fremake.command()
+@click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
+def checkout(uppercase):
+    statement = "execute fre make checkout script" 
+    if uppercase:
+        statement = statement.upper()
+    click.echo(statement)
+    
+@fremake.command()
+def compile():
+    click.echo("execute fre make compile script")
+
+@fremake.command()
+def container():
+    click.echo("execute fre make container script")
+
+@fremake.command()
+def list():
+    click.echo("execute fre make list script")
+
+
+# # first function, hello
+# @click.command()
+# @click.argument('name')
+# @click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
+# def hello(name, uppercase):
+#     #click.echo(f"Hello, {name}!")
+#     greeting = f"Hello, {name}!"
+#     if uppercase:
+#         greeting = greeting.upper()
+#     click.echo(greeting)
+# 
+# # second function, goodbye
+# @click.command()
+# @click.argument('name')
+# @click.option('--uppercase', '-u', is_flag=True, help = 'Print name in uppercase.')
+# def goodbye(name, uppercase):
+#     #click.echo(f"Hello, {name}!")
+#     farewell = f"Goodbye, {name}!"
+#     if uppercase:
+#         farewell = farewell.upper()
+#     click.echo(farewell)
+# 
+# # add your functions to the click group
+# cli.add_command(hello)
+# cli.add_command(goodbye)
+
+if __name__ == '__main__':
+    fre()
