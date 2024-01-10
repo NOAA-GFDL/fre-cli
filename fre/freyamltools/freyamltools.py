3be716a0a12bbb09c31e5ad2628245d7f36c179b
Bennett Chang <bcc2761@g.rit.edu>
2024-01-10 17:15:42 -0500

    added barebone code for yamltools

---
+++ fre/freyamltools/freyamltools.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+## \date 2024
+## \author Bennett Chang
+## \description 
+
+import os
+from pathlib import Path
+import yaml
+import click
+
+from fre.freyamltools.yamltoolsfile import *
+
+@click.group()
+def yamltools():
+    pass 
+
+@yamltools.command()
+@click.option('--uppercase', '-u', is_flag=True, help = 'Print statement in uppercase.')
+@click.pass_context
+def testfunction(context, uppercase):
+    """ - Execute fre yamltools function """
+    context.forward(function)
+
+if __name__ == "__main__":
+    test()
