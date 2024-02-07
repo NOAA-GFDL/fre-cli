f056f2fb4e31aa758e35cafc3a5c16ed75b294fe
Dana Singh <Dana.Singh@an202.princeton.rdhpcs.noaa.gov>
2024-02-07 15:50:43 -0500

    #9 Update `config2.py`
    - add yaml validation
    - add click usage
    - include all source information for components in regrid rose-app (until rose is retired)

--- fre/frepp/configUpdates/config2.py
+++ fre/frepp/configUpdates/config2.py
@@ -6,15 +6,37 @@ import click
 from jsonschema import validate, ValidationError, SchemaError
 import json
 
-file="pp.yaml"
-with open(file,'r') as f:
+######VALIDATE#####
+package_dir = os.path.dirname(os.path.abspath(__file__))
+schema_path = os.path.join(package_dir, 'schema-pp.json')
+
+def validateYaml(file):
+  # Load the json schema: .load() (vs .loads()) reads and parses the json in one
+  with open(schema_path) as s:
+    schema = json.load(s)
+
+  # Validate yaml
+  # If the yaml is not valid, the schema validation will raise errors and exit
+  if validate(instance=file,schema=schema) == None:
+    print("YAML VALID")
+
+###################
+@click.command()
+@click.option("-y",
+              type=str,
+              help="YAML file to be used for parsing",
+              required=True)
+
+#maybe change name to parseYaml
+def yamlInfo(y):
+#file="pp.yaml"
+  with open(y,'r') as f:
     y=yaml.safe_load(f)
+    yml = validateYaml(y)
 
-#first dictionary 
-#print(y)
-
+#PARSE YAML
 ## Write the rose-suite-exp configuration
-for key,value in y.items():
+  for key,value in y.items():
     #print(key)
 
     ## Add: check paths?
@@ -58,7 +80,7 @@ for key,value in y.items():
                 elif "pp_stop" in configkey:
                   f.write(f'{k}="{configvalue}OOTZ"\n\n')
                 else:
-                  f.write(f'{k}="{configvalue}"\n\n')     
+                  f.write(f'{k}="{configvalue}"\n\n')
 
 ##if value of type is in remap AND regrid grid value in remap rose app=regrid-xy
     if key == "regrid-remap-info":
@@ -68,40 +90,54 @@ for key,value in y.items():
             # Create/write remap rose app
             with open(remap_roseapp,'a') as f:
               if compkey == "type": 
-                f.write(f"\n[{compvalue}]\n")
+                f.write(f"\n\n[{compvalue}]\n")
 
                 #if xyInterp doesnt exist, grid is native
                 if i.get("xyInterp") == None:
                   f.write(f"grid=native\n")
-
                 #in xyInterp exists, component can be regridded
                 elif i.get("xyInterp") != None:
                   f.write("grid=regrid-xy\n")                       
 
-#              if key != "type":
-#                f.write(f"{key}={value}\n")
-
               elif compkey == "sources":
-                f.write(f"{compkey}={compvalue}\n")
+                f.write(f"{compkey}={compvalue} ")
+              elif compkey == "timeSeries":
+                #print(compvalue)
+                for i in compvalue:
+                  #print(i)  
+                  for key,value in i.items():
+                    if key == "source":
+                      f.write(f"{value} ")
 
             # Create/write regrid rose app
             with open(regrid_roseapp,'a') as f:
+              # if xyInterp is defined, that is a regridded component
               if i.get("xyInterp") != None:
                 if compkey == "type":
                   f.write(f"\n[{compvalue}]\n")
                   if "atmos" in compvalue:
-                    f.write(f"inputRealm=atmos\n")
+                    f.write("inputRealm=atmos\n")
                   elif "land" in compvalue:
-                    f.write(f"inputRealm=land\n")
+                    f.write("inputRealm=land\n")
+                  elif "river" in compvalue:
+                    f.write("inputRealm=land\n")
                   elif "ocean" in compvalue:
-                    f.write(f"inputRealm=ocean\n")
-                elif compkey == "sources":
-                  f.write(f"{compkey}={compvalue}\n")
-                elif compkey == "sourceGrid":
+                    f.write("inputRealm=ocean\n")
+                  elif "aerosol" or "tracer" in compvalue:
+                    f.write("inputRealm=aerosol\n")
+                elif compkey == "sourceGrid": 
                   f.write(f"inputGrid={compvalue}\n")
                 elif compkey == "interpMethod":
                   f.write(f"{compkey}={compvalue}\n")
-
-##TO-DO
-#validation
-#fix sources
+                elif compkey == "sources":
+                  f.write(f"{compkey}={compvalue} ")
+                  if i["timeSeries"]:
+                    for elem in i['timeSeries']:
+                      for key,value in elem.items():
+                        if key == "source":
+                          f.write(f"{value} ")
+                  f.write("\n")
+                    
+# Use parseyaml function to parse created edits.yaml
+if __name__ == '__main__':
+    yamlInfo()
