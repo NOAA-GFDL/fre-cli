1f53b1b73f41fa7630f0290d5b3a970c4decd206
Bennett Chang <bcc2761@g.rit.edu>
2024-02-26 14:08:51 -0500

    setup.py version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fre-cli',
-    version='0.1.0',
+    version='0.1.3',
     description='Command Line Interface for FRE commands',
     author='Bennett Chang',
     author_email='Bennett.Chang@noaa.gov',
