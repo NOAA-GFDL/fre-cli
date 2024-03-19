9ebb71ae700cb58397e44f8f0d6a53b7e9427e9e
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2024-03-19 16:21:52 -0400

    unneeded but harmless cleanup

--- fre/frecmor/frecmor.py
+++ fre/frecmor/frecmor.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python3
-## \date 2023
-## \author Bennett Chang
-## \description Script for frepp is used to post process outputs obtained from other fre commands.
-
-import click
-
 from fre.frecmor.CMORmixer import *
