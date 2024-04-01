8c3ac3f8e04583b38cbfdc1a2bcfcb70fc8611c1
Ciheim Brown <Ciheim.Brown@ldt-4788508.gfdl.noaa.gov>
2024-04-01 14:21:24 -0400

    Fixing theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'renku_sphinx_theme'
+html_theme = 'renku'
 html_static_path = ['_static']
