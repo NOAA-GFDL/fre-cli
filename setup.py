08ad5b38d0b7bee5a0ce6808df8ea4f0b274d383
Bennett Chang <98476720+bcc2761@users.noreply.github.com>
2024-06-13 12:30:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='fre-cli',
-    version=version_from_git(),
+    version=get_git_version(),
     description='Command Line Interface for FRE commands',
     author='MSD Workflow Team, Bennett Chang, Dana Singh, Chris Blanton',
     author_email='oar.gfdl.workflow@noaa.gov',
