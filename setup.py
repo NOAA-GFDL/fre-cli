1acae52eacfccf9f8031f7aef431c7fb17c7f2a7
Bennett Chang <98476720+bcc2761@users.noreply.github.com>
2024-06-13 12:40:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,8 @@
 import subprocess
 
-def get_git_version():
-  try:
-    # Get the latest tag using git describe
-    output = subprocess.check_output(['git', 'describe', '--abbrev=0']).decode('utf-8').strip()
-    # Split the output to get the tag (assuming valid format)
-    return output.split('-')[0]
-  except subprocess.CalledProcessError:
-    # Fallback to a default version if git fails
-    return "0.0.0"
-from setuptools import setup, find_namespace_packages
-
 setup(
     name='fre-cli',
-    version=get_git_version(),
+    version='0.1.7',
     description='Command Line Interface for FRE commands',
     author='MSD Workflow Team, Bennett Chang, Dana Singh, Chris Blanton',
     author_email='oar.gfdl.workflow@noaa.gov',
