3c7de948938b039e340b3a848bc9735e4a672ecf
Carolyn Whitlock <carolyn.whitlock@noaa.gov>
2024-06-14 11:05:12 -0400

    fixing tab/space mixup in checkoutScript

--- fre/pp/checkoutScript.py
+++ fre/pp/checkoutScript.py
@@ -24,39 +24,39 @@ def _checkoutTemplate(experiment, platform, target, branch='main'):
     """
     remote = "https://gitlab.gfdl.noaa.gov/fre2/workflows/postprocessing.git"
     src_dir = make_src_dir(experiment, platform, target)
-	if not os.path.exists(src_dir):
-		clone_from_remote(src_dir, branch, remote)
-	else:
-		check_preexist_match(src_dir, branch, remote)
-	return 0
+    if not os.path.exists(src_dir):
+        clone_from_remote(src_dir, branch, remote)
+    else:
+        check_preexist_match(src_dir, branch, remote)
+    return 0
 
 
 #############################################
 def make_src_dir(experiment, platform, target):
-	'''
-	Makes official location of cylc-src dir.
-	Separating into a function because we might check out to a temporary dir - 
-	and currently it's hard-coded in a couple functions to ~/cylc-src 
-	'''
-	name = f"{experiment}__{platform}__{target}"
-	src_root = os.path.join(os.path.expanduser("~/cylc-src"), name)
-	return(src_root)
-	
+    '''
+    Makes official location of cylc-src dir.
+    Separating into a function because we might check out to a temporary dir - 
+    and currently it's hard-coded in a couple functions to ~/cylc-src 
+    '''
+    name = f"{experiment}__{platform}__{target}"
+    src_root = os.path.join(os.path.expanduser("~/cylc-src"), name)
+    return(src_root)
+    
 def check_preexist_match(src_dir, branch, remote):
-	os.chdir(src_dir)
-	git_branch_cmd = "git branch --show-current"
-	#If it exits with an error, odds are it isn't a git repo - advise re-checking out
-	if git_branch != branch:
-		stop("Error in wrapperscript: checking out a branch '{branch}' that does not match existing branch '{git_branch}' in {src_dir}!")
-	else:
-		git_loc_commit_cmd = "git log | head -n 1 | cut -d ' ' -f 2"
-		git_rem_commit_cmd = """
-		                     git remote add origin {remote_repo};
-		                     git fetch
-		                     git log origin/{branch} | head -n 1 | cut -d ' ' -f 2
-		                     """
-		 if git_loc_commit != git_rem_commit:
-		 	stop("Error in check_preexist: git repo located at {src_dir} has a latest commit in {branch} that does not match the remote! Please resolve or remove and re-clone the dir at {src_dir}"
+    os.chdir(src_dir)
+    git_branch_cmd = "git branch --show-current"
+    #If it exits with an error, odds are it isn't a git repo - advise re-checking out
+    if git_branch != branch:
+        stop("Error in wrapperscript: checking out a branch '{branch}' that does not match existing branch '{git_branch}' in {src_dir}!")
+    else:
+        git_loc_commit_cmd = "git log | head -n 1 | cut -d ' ' -f 2"
+        git_rem_commit_cmd = """
+                             git remote add origin {remote_repo};
+                             git fetch
+                             git log origin/{branch} | head -n 1 | cut -d ' ' -f 2
+                             """
+         if git_loc_commit != git_rem_commit:
+             stop("Error in check_preexist: git repo located at {src_dir} has a latest commit in {branch} that does not match the remote! Please resolve or remove and re-clone the dir at {src_dir}"
 
 def clone_from_remote(src_dir, branch, remote):
     click.echo("cloning experiment into directory " + directory + "/" + name)
