a7d5d4333365f0551756d49cfd15fdd877f5b67c
Ian <ilaflott@gmail.com>
2024-07-16 12:30:20 -0400

    Update conf.py
    @singhd789 suggests changing the theme- it's likely the `renku` theme that hasn't been updated since aug 2023 is using a key that is no longer needed by `sphinx` and now breaks the workflow as of v4.7.7.
    
    changing theme from `renku` to `rtd`

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'renku'
+#html_theme = 'renku'
+html_theme = 'rtd'
 html_static_path = ['_static']
