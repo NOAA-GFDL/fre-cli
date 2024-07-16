160fd94972845fb85fc11248a73af59181699556
Ian <ilaflott@gmail.com>
2024-07-16 12:39:07 -0400

    Update conf.py
    back to old theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'renku'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'renku'
+#html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
