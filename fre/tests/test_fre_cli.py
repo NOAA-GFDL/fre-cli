1920f261ee9e77b71fedbdc36ee6cf48df444112
Carolyn Whitlock <carolyn.whitlock@noaa.gov>
2024-07-23 20:48:22 -0400

    testing github yaml running

--- fre/tests/test_fre_cli.py
+++ fre/tests/test_fre_cli.py
@@ -5,6 +5,11 @@ from fre import fre
 from click.testing import CliRunner
 runner = CliRunner()
 
+#tests are structured in the manner of: 
+#https://click.palletsprojects.com/en/8.1.x/testing/
+#general intent for these tests is that each fre tool has 3 commandline tests:
+#app, catalog, check,cmor, list, make, pp, run
+
 
 def test_cli_fre():
     result = runner.invoke(fre.fre)
@@ -23,3 +28,16 @@ def test_cli_fre_option_dne():
     #print(f'exit code of runner result is {result.exit_code}')
     #print(f'output of runner result is {result.output}')
     assert result.exit_code == 2
+    
+def test_cli_fre_catalog():
+    result = runner.invoke(fre.fre, args=['--help', "catalog"])
+    assert result.exit_code == 0
+
+'''    
+def test_cli_fre_catalog_help():
+    assert result.exit_code == 0
+    
+def test_cli_fre_catalog_opt_dne():
+    assert result.exit_code == 2
+
+'''
