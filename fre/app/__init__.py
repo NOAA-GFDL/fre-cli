d3f85499135af074bb93bc52c453397c19e234ee
Ian Laflotte <ian.laflotte@noaa.gov>
2024-08-12 12:47:20 -0400

    always fun to hit a difference in functioning between my local environment and github's CI/CD
    suspecting import problems with __init__.py file in fre/app/. everything works fine locally.
    
    committing + pushing this while clobbering local folder/caches to start from a cleaner spot

--- fre/app/__init__.py
+++ fre/app/__init__.py
@@ -1,5 +1,6 @@
 ''' for fre.app imports '''
-from .mask_atmos_plevel import mask_atmos_plevel_subtool
+#from .mask_atmos_plevel import mask_atmos_plevel_subtool
 from .freapp import app_cli
 
-__all__ = ["mask_atmos_plevel_subtool", "app_cli"]
+#__all__ = ["mask_atmos_plevel_subtool", "app_cli"]
+__all__ = ["app_cli"]
