3dc1e33a0c4f62c877dfb487aa21db26e5fcdcfb
Ian Laflotte <ian.laflotte@noaa.gov>
2024-08-12 12:53:38 -0400

    oh, the problem is i deleted the tool -_-.
    adding back in...

--- fre/app/__init__.py
+++ fre/app/__init__.py
@@ -1,6 +1,4 @@
 ''' for fre.app imports '''
-#from .mask_atmos_plevel import mask_atmos_plevel_subtool
 from .freapp import app_cli
 
-#__all__ = ["mask_atmos_plevel_subtool", "app_cli"]
 __all__ = ["app_cli"]
