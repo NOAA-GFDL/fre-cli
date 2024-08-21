30d538cbb65d545fdcf8017a50ee19e845ad4256
Dana Singh <Dana.Singh@noaa.gov>
2024-08-21 13:21:45 -0400

    #161 Create output directory

--- fre/yamltools/tests/test_combine_yamls.py
+++ fre/yamltools/tests/test_combine_yamls.py
@@ -1,13 +1,24 @@
 import os
 from pathlib import Path
 import pytest
+import shutil
 from fre.yamltools import combine_yamls as cy
 
 ## SET-UP
-# Set example yaml paths, input directory
+# Set example yaml paths, input directory, output directory
 CWD = Path.cwd()
 yamls_dir = Path("fre/yamltools/tests/AM5_example")
 in_dir = Path(f"{CWD}/{yamls_dir}")
+
+# Create output directory
+out_dir = Path(f"{CWD}/fre/yamltools/tests/combine_yamls_out")
+
+# If output directory exists, remove and create again 
+if Path(out_dir).exists:
+    shutil.rmtree(out_dir)
+    Path(out_dir).mkdir(parents=True,exist_ok=True)
+else:
+    Path(out_dir).mkdir(parents=True,exist_ok=True)
 
 # Set what would be click options
 EXPERIMENT = "c96L65_am5f7b12r1_amip"
@@ -50,10 +61,18 @@ def test_merged_yamls():
     Check for the creation of the combined-[experiment] yaml 
     Check that the model yaml was merged into the combined yaml
     """
-    os.chdir(in_dir)
+    # Model yaml path
+    modelyaml = Path(f"{in_dir}/am5.yaml")
 
-    modelyaml = Path(f"{CWD}/{yamls_dir}/am5.yaml")
-
+    # Merge the yamls
     cy._consolidate_yamls(modelyaml,EXPERIMENT, PLATFORM, TARGET)
 
-    assert Path(f"{CWD}/{yamls_dir}/combined-c96L65_am5f7b12r1_amip.yaml").exists()
+    # Move combined yaml to output location
+    shutil.move(f"{CWD}/combined-{EXPERIMENT}.yaml", out_dir)
+
+    # Check that the combined yaml exists
+    assert Path(f"{out_dir}/combined-{EXPERIMENT}.yaml").exists()
+
+##TO-DO:
+# - check for correct yaml merging
+# - 
