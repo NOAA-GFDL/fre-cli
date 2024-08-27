03db8ba876083b9f78b379049f1a3cb65fb81c72
Dana Singh <Dana.Singh@noaa.gov>
2024-08-27 13:39:26 -0400

    #141 Fix up pylint messages

--- fre/make/gfdlfremake/platformfre.py
+++ fre/make/gfdlfremake/platformfre.py
@@ -1,102 +1,113 @@
 import yaml
 
 class platforms ():
-## \param self The platform yaml object
-## \param fname The path to the platform yaml file
-## \param v the fre variables defined in the model Yaml
- def __init__(self,yamlFile): #,v):
-     self.yaml = yamlFile
+    def __init__(self,fname):
+        """
+        Param:
+            - self The platform yaml object
+            - fname The path to the platform yaml file
+        """
+        self.yaml = fname
 
-## Check the yaml for errors/omissions
-## Loop through the platforms
-     for p in self.yaml:
-## Check the platform name
-          try:
-               p["name"]
-          except:
-               print("At least one of the platforms is missing a name in "+fname+"\n")
-               raise
-## Check the compiler
-          try:
-               p["compiler"]
-          except:
-               print ("You must specify a compiler in your "+p["name"]+" platform in the file "+fname+"\n")
-               raise
-## Check for the Fortran (fc) and C (cc) compilers
-          try:
-               p["fc"]
-          except:
-               print ("You must specify the name of the Fortran compiler as fc on the "+p["name"]+" platform in the file "+fname+"\n")
-               raise
-          try:
-               p["cc"]
-          except:
-               print ("You must specify the name of the Fortran compiler as cc on the "+p["name"]+" platform in the file "+fname+"\n")
-               raise
-## Check for modules to load
-          try:
-               p["modules"]
-          except:
-               p["modules"]=[""]
-## Check for modulesInit to set up the modules environment
-          try:
-               p["modulesInit"]
-          except:
-               p["modulesInit"]=[""]
-## Get the root for the build
-          try:
-               p["modelRoot"]
-          except:
-               p["modelRoot"] = "/apps"
-## Check if we are working with a container and get the info for that
-          try:
-               p["container"]
-          except:
-               p["container"] = False
-               p["RUNenv"] = ""
-               p["containerBuild"] = ""
-               p["containerRun"] = ""
-          if p["container"]:
-## Check the container builder
-               try:
+        ## Check the yaml for errors/omissions
+        ## Loop through the platforms
+        for p in self.yaml:
+            ## Check the platform name
+            try:
+                p["name"]
+            except:
+                print("At least one of the platforms is missing a name in "+fname+"\n")
+                raise
+            ## Check the compiler
+            try:
+                p["compiler"]
+            except:
+                print ("You must specify a compiler in your "+p["name"]+" platform in the file "+fname+"\n")
+                raise
+            ## Check for the Fortran (fc) and C (cc) compilers
+            try:
+                p["fc"]
+            except:
+                print ("You must specify the name of the Fortran compiler as fc on the "+p["name"]+" platform in the file "+fname+"\n")
+                raise
+            try:
+                p["cc"]
+            except:
+                print ("You must specify the name of the Fortran compiler as cc on the "+p["name"]+" platform in the file "+fname+"\n")
+                raise
+            ## Check for modules to load
+            try:
+                p["modules"]
+            except:
+                p["modules"]=[""]
+            ## Check for modulesInit to set up the modules environment
+            try:
+                p["modulesInit"]
+            except:
+                p["modulesInit"]=[""]
+            ## Get the root for the build
+            try:
+                p["modelRoot"]
+            except:
+                p["modelRoot"] = "/apps"
+            ## Check if we are working with a container and get the info for that
+            try:
+                p["container"]
+            except:
+                p["container"] = False
+                p["RUNenv"] = ""
+                p["containerBuild"] = ""
+                p["containerRun"] = ""
+            if p["container"]:
+                ## Check the container builder
+                try:
                     p["containerBuild"]
-               except:
+                except:
                     print ("You must specify the program used to build the container (containerBuild) on the "+p["name"]+" platform in the file "+fname+"\n")
                     raise
-               if p["containerBuild"] != "podman" and p["containerBuild"] != "docker":
+                if p["containerBuild"] != "podman" and p["containerBuild"] != "docker":
                     raise ValueError("Container builds only supported with docker or podman, but you listed "+p["containerBuild"]+"\n")
-## Check for container environment set up for RUN commands
-               try:
+                ## Check for container environment set up for RUN commands
+                try:
                     p["RUNenv"]
-               except:
+                except:
                     p["RUNenv"] = ""
-## Check the container runner
-               try:
+                ## Check the container runner
+                try:
                     p["containerRun"]
-               except:
+                except:
                     print ("You must specify the program used to run the container (containerRun) on the "+p["name"]+" platform in the file "+fname+"\n")
                     raise
-               if p["containerRun"] != "apptainer" and p["containerRun"] != "singularity":
+                if p["containerRun"] != "apptainer" and p["containerRun"] != "singularity":
                     raise ValueError("Container builds only supported with apptainer, but you listed "+p["containerRun"]+"\n")
-## set the location of the mkTemplate.  In a container, it uses the hpc-me template cloned from mkmf
-               p["mkTemplate"] = "/apps/mkmf/templates/hpcme-intel21.mk"
-          else:
-               try:
+                ## set the location of the mkTemplate.
+                ## In a container, it uses the hpc-me template cloned from mkmf
+                p["mkTemplate"] = "/apps/mkmf/templates/hpcme-intel21.mk"
+            else:
+                try:
                     p["mkTemplate"]
-               except:
+                except:
                     raise ValueError("The non-container platform "+p["name"]+" must specify a mkTemplate \n")
 
-## \brief Checks if the platform yaml has the named platform
- def hasPlatform(self,name):
-     for p in self.yaml:
-          if p["name"] == name:
-               return True
-     return False
-## \brief Get the platform yaml
- def getPlatformsYaml(self):
-     return self.yaml
-## \brief Get the platform information from the name of the platform
- def getPlatformFromName(self,name):
-     for p in self.yaml:
-          if p["name"] == name:
-               return (p["compiler"], p["modules"], p["modulesInit"], p["fc"], p["cc"], p["modelRoot"],p["container"], p["mkTemplate"],p["containerBuild"], p["containerRun"], p["RUNenv"])
+    def hasPlatform(self,name):
+        """
+        Brief: Checks if the platform yaml has the named platform
+        """
+        for p in self.yaml:
+            if p["name"] == name:
+                return True
+        return False
+
+    def getPlatformsYaml(self):
+        """
+        Brief: Get the platform yaml
+        """
+        return self.yaml
+
+    def getPlatformFromName(self,name):
+        """
+        Brief: Get the platform information from the name of the platform
+        """
+        for p in self.yaml:
+            if p["name"] == name:
+                return (p["compiler"], p["modules"], p["modulesInit"], p["fc"], p["cc"], p["modelRoot"],p["container"], p["mkTemplate"],p["containerBuild"], p["containerRun"], p["RUNenv"])
--- fre/make/gfdlfremake/yamlfre.py
+++ fre/make/gfdlfremake/yamlfre.py
@@ -1,27 +1,33 @@
 import os
-from pathlib import Path
-import shutil
-import yaml
 import json
+import yaml
 from jsonschema import validate, ValidationError, SchemaError
 from . import platformfre
 
-## Open the yaml file and parse as fremakeYaml
-## \param fname the name of the yaml file to parse
 def parseCompile(fname):
-# Open the yaml file and parse as fremakeYaml
-     with open(fname, 'r') as yamlfile:
-          y = yaml.safe_load(yamlfile)
+    """
+    Brief: Open the yaml file and parse as fremakeYaml
+    Param:
+        - fname the name of the yaml file to parse
+    """
+    # Open the yaml file and parse as fremakeYaml
+    with open(fname, 'r') as yamlfile:
+        y = yaml.safe_load(yamlfile)
 
-     return y 
+    return y
 
-## \brief Checks the yaml for variables.  Required variables will dump and error.  Non-required variables will 
+## \brief Checks the yaml for variables. Required variables will dump and error. Non-required variables will
 ## set a default value for the variable
-## \param var A variable in the yaml
-## \param val a default value for var
-## \param req if true, the variable is required in the yaml and an exception will be raised
-## \param err An error message to print if the variable is required and doesn't exist
 #def yamlVarCheck(var,val="",req=False,err="error"):
+#    """
+#    Brief: Checks the yaml for variables. Required variables will dump and error.
+#           Non-required variables will set a default value for the variable
+#    Param:
+#        - var A variable in the yaml
+#        - val a default value for var
+#        - req if true, the variable is required in the yaml and an exception will be raised
+#        - err An error message to print if the variable is required and doesn't exist
+#    """
 #     try:
 #          var
 #     except:
@@ -31,113 +37,124 @@ def parseCompile(fname):
 #          else:
 #               var = val
 
-## This will read the compile yaml for FRE and then fill in any of the missing non-required variables
 class compileYaml():
-## Read get the compile yaml and fill in the missing pieces
-## \param self the compile Yaml object
-## \yamlFile The path to the compile yaml file
- def __init__(self,yamlFile):
-     self.yaml = yamlFile
+    """
+    Brief: This will read the compile yaml for FRE and then fill in any of the missing non-required variables
+    """
+    def __init__(self,yamlFile):
+        """
+        Brief: Read get the compile yaml and fill in the missing pieces
+        Param:
+            - self the compile Yaml object
+            - yamlFile The path to the compile yaml file
+        """
+        self.yaml = yamlFile
 
-     ## Check the yaml for required things
-     ## Check for required experiment name
-     try:
-          self.yaml["experiment"]
-     except:
-          print("You must set an experiment name to compile \n")
-          raise
-     ## Check for optional libraries and packages for linking in container
-     try: 
-          self.yaml["container_addlibs"]
-     except:
-          self.yaml["container_addlibs"]=""
-     ## Check for optional libraries and packages for linking on bare-metal system
-     try:
-          self.yaml["baremetal_linkerflags"]
-     except:
-          self.yaml["baremetal_linkerflags"]=""
-     ## Check for required src
-     try:
-          self.yaml["src"]
-     except:
-          print("You must set a src to specify the sources in modelRoot/"+self.yaml["experiment"]+"\n")
-          raise
-     ## Loop through the src array
-     for c in self.yaml['src']:
-     ## Check for required componenet name
-          try:
-               c['component']
-          except:
-               print("You must set the 'componet' name for each src component")
-               raise
-     ## Check for required repo url
-          try:
-               c['repo']
-          except:
-               print("'repo' is missing from the component "+c['component']+" in "+self.yaml["experiment"]+"\n")
-               raise
-     # Check for optional branch. Otherwise set it to blank
-          try:
-               c['branch']
-          except:
-               c['branch']=""
-     # Check for optional cppdefs. Otherwise set it to blank
-          try:
-               c['cppdefs']
-          except:
-               c['cppdefs']=""
-     # Check for optional doF90Cpp. Otherwise set it to False
-          try:
-               c['doF90Cpp']
-          except:
-               c['doF90Cpp']=False
-     # Check for optional additional instructions. Otherwise set it to blank
-          try:
-               c['additionalInstructions']
-          except:
-               c['additionalInstructions']=""
-     # Check for optional paths. Otherwise set it to blank
-          try:
-               c['paths']
-          except:
-               c['paths']=[c['component']]
-     # Check for optional requires. Otherwise set it to blank
-          try:
-               c['requires']
-          except:
-               c['requires']=[]
-     # Check for optional overrides. Otherwise set it to blank
-          try:
-               c['makeOverrides']
-          except:
-               c['makeOverrides']=""
-     # Check for optional flags. Otherwise set it to blank. 
-          try:
-               c["otherFlags"]
-          except:
-               c["otherFlags"]=""
+        ## Check the yaml for required things
+        ## Check for required experiment name
+        try:
+            self.yaml["experiment"]
+        except:
+            print("You must set an experiment name to compile \n")
+            raise
+        ## Check for optional libraries and packages for linking in container
+        try:
+            self.yaml["container_addlibs"]
+        except:
+            self.yaml["container_addlibs"]=""
+        ## Check for optional libraries and packages for linking on bare-metal system
+        try:
+            self.yaml["baremetal_linkerflags"]
+        except:
+            self.yaml["baremetal_linkerflags"]=""
+        ## Check for required src
+        try:
+            self.yaml["src"]
+        except:
+            print("You must set a src to specify the sources in modelRoot/"+self.yaml["experiment"]+"\n")
+            raise
+        ## Loop through the src array
+        for c in self.yaml['src']:
+        ## Check for required componenet name
+            try:
+                c['component']
+            except:
+                print("You must set the 'componet' name for each src component")
+                raise
+            ## Check for required repo url
+            try:
+                c['repo']
+            except:
+                print("'repo' is missing from the component "+c['component']+" in "+self.yaml["experiment"]+"\n")
+                raise
+            # Check for optional branch. Otherwise set it to blank
+            try:
+                c['branch']
+            except:
+                c['branch']=""
+            # Check for optional cppdefs. Otherwise set it to blank
+            try:
+                c['cppdefs']
+            except:
+                c['cppdefs']=""
+            # Check for optional doF90Cpp. Otherwise set it to False
+            try:
+                c['doF90Cpp']
+            except:
+                c['doF90Cpp']=False
+            # Check for optional additional instructions. Otherwise set it to blank
+            try:
+                c['additionalInstructions']
+            except:
+                c['additionalInstructions']=""
+            # Check for optional paths. Otherwise set it to blank
+            try:
+                c['paths']
+            except:
+                c['paths']=[c['component']]
+            # Check for optional requires. Otherwise set it to blank
+            try:
+                c['requires']
+            except:
+                c['requires']=[]
+            # Check for optional overrides. Otherwise set it to blank
+            try:
+                c['makeOverrides']
+            except:
+                c['makeOverrides']=""
+            # Check for optional flags. Otherwise set it to blank.
+            try:
+                c["otherFlags"]
+            except:
+                c["otherFlags"]=""
 
-## Returns the compile yaml
- def getCompileYaml(self):
-     try:
-          self.yaml
-     except:
-          print ("You must initialize the compile YAML object before you try to get the yaml \n")
-          raise
-     return self.yaml
-
-#########################################################################################################################
-## \description This will take the models yaml file which has a list of the sub yaml files and combine them into the 
-## full freyaml that can be used and checked
-# platformYaml: platforms.yaml
-# compileYaml: compile.yaml
+    def getCompileYaml(self):
+        """
+        Brief: Returns the compile yaml
+        """
+        try:
+            self.yaml
+        except:
+            print ("You must initialize the compile YAML object before you try to get the yaml \n")
+            raise
+        return self.yaml
 
 class freyaml():
-## \param self The freyaml object
-## \param combinedyaml The name of the combined yaml file
- def __init__(self,combinedyaml):
-     # Parse
-     self.combined = parseCompile(combinedyaml)
+    """
+    Brief: This will take the models yaml file which has a list of the sub yaml files and combine them into the
+    Note:
+        - full freyaml that can be used and checked
+        - platformYaml: platforms.yaml
+        - compileYaml: compile.yaml
+    """
+    def __init__(self,combinedyaml):
+        """
+        Param:
+            - self The freyaml object
+            - combinedyaml The name of the combined yaml file
+        """
+        # Parse
+        self.combined = parseCompile(combinedyaml)
 
 ## Validate the YAML
 #     fremake_package_dir = os.path.dirname(os.path.abspath(__file__))
@@ -145,23 +162,27 @@ class freyaml():
 #     with open(schema_path, 'r') as f:
 #         s = f.read()
 #     schema = json.loads(s)
-#     validate(instance=self.combined,schema=schema) 
+#     validate(instance=self.combined,schema=schema)
 #     print("\nCOMBINED YAML VALID")
 
-     #get compile info
-     self.compiledict = self.combined.get("compile")
-     self.compile = compileYaml(self.compiledict)
-     self.compileyaml = self.compile.getCompileYaml()
- 
-     #get platform info
-     self.platformsdict = self.combined.get("platforms")
-     self.platforms = platformfre.platforms(self.platformsdict)
-     self.platformsyaml = self.platforms.getPlatformsYaml()
+        #get compile info
+        self.compiledict = self.combined.get("compile")
+        self.compile = compileYaml(self.compiledict)
+        self.compileyaml = self.compile.getCompileYaml()
 
-## Returns the compile yaml
- def getCompileYaml(self):
-     return self.compileyaml
+        #get platform info
+        self.platformsdict = self.combined.get("platforms")
+        self.platforms = platformfre.platforms(self.platformsdict)
+        self.platformsyaml = self.platforms.getPlatformsYaml()
 
-## Returns the compile yaml
- def getPlatformsYaml(self):
-     return self.platformsyaml
+    def getCompileYaml(self):
+        """
+        Brief: Returns the compile yaml
+        """
+        return self.compileyaml
+
+    def getPlatformsYaml(self):
+        """
+        Brief: Returns the compile yaml
+        """
+        return self.platformsyaml
