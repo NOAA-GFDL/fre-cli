33653f9ae76278dd5ad25d536def9895761726bf
Ciheim Brown <Ciheim.Brown@ldt-4788508.gfdl.noaa.gov>
2024-08-28 21:30:15 -0400

    Currently messing with different nccmp calls... error here... returning later

--- fre/tests/test_fre_cmor_cli.py
+++ fre/tests/test_fre_cmor_cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ''' test "fre cmor" calls '''
 
+import os
+
 from click.testing import CliRunner
 
 from fre import fre
@@ -33,3 +35,4 @@ def test_cli_fre_cmor_run():
     result = runner.invoke(fre.fre, args=["cmor", "run", "--indir", indir, "--varlist", varlist, "--table_config", table_config, "--exp_config", exp_config,"--outdir",  outdir])
     assert result.exit_code == 0
 
+    assert os.system("nccmp -f -m /nbhome/Ciheim.Brown/outdir/CMIP6/CMIP6/ISMIP6/PCMDI/PCMDI-test-1-0/piControl-withism/r3i1p1f1/Omon/sos/gn/v20240826/sos_Omon_PCMDI-test-1-0_piControl-withism_r3i1p1f1_gn_199307-199807.nc /nbhome/Ciheim.Brown/where-the-sos-lives/ocean_monthly_1x1deg.199301-199712.sos.nc") == 1
