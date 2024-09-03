74556d2e13c46f57e1456c682f4f4067b2e650a3
Ciheim Brown <Ciheim.Brown@ldt-4788508.gfdl.noaa.gov>
2024-09-03 15:08:43 -0400

    Latest state of test

--- fre/tests/test_fre_cmor_cli.py
+++ fre/tests/test_fre_cmor_cli.py
@@ -2,6 +2,7 @@
 ''' test "fre cmor" calls '''
 
 import subprocess
+import netCDF4 as nc
 
 from click.testing import CliRunner
 
@@ -32,10 +33,12 @@ def test_cli_fre_cmor_run_case1():
     exp_config = '/nbhome/Ciheim.Brown/CMOR_input_example.json'
     outdir = '/nbhome/Ciheim.Brown/outdir'
 
+    subprocess.run(["mkdir", "-p", outdir+'/tmp'])
     result = runner.invoke(fre.fre, args=["cmor", "run", "--indir", indir, "--varlist", varlist, "--table_config", table_config, "--exp_config", exp_config,"--outdir",  outdir])
     assert result.exit_code == 0
 
 def test_cli_fre_cmor_run_case2(capfd):
-    assert subprocess.run(["nccmp", "-f", "-m", "/nbhome/Ciheim.Brown/outdir/CMIP6/CMIP6/ISMIP6/PCMDI/PCMDI-test-1-0/piControl-withism/r3i1p1f1/Omon/sos/gn/v20240829/sos_Omon_PCMDI-test-1-0_piControl-withism_r3i1p1f1_gn_199307-199807.nc", "/nbhome/Ciheim.Brown/where-the-sos-lives/ocean_monthly_1x1deg.199301-199712.sos.nc"]).returncode == 1
+
+    assert subprocess.run(["nccmp -f -m /nbhome/Ciheim.Brown/outdir/CMIP6/CMIP6/ISMIP6/PCMDI/PCMDI-test-1-0/piControl-withism/r3i1p1f1/Omon/sos/gn/*/sos_Omon_PCMDI-test-1-0_piControl-withism_r3i1p1f1_gn_199307-199807.nc /nbhome/Ciheim.Brown/where-the-sos-lives/ocean_monthly_1x1deg.199301-199712.sos.nc"],shell=True).returncode == 1
     out, err = capfd.readouterr()
-    subprocess.run(["rm", "-rf", "/nbhome/Ciheim.Brown/outdir/CMIP6/CMIP6/"])
+    #subprocess.run(["rm", "-rf", "/nbhome/Ciheim.Brown/outdir/CMIP6/CMIP6/"])
