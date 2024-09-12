397915552d260f9b87392913315075e09f2b2349
Ian Laflotte <ian.laflotte@noaa.gov>
2024-09-12 12:58:03 -0400

    change exit code check for tests checking fre pp <subtool> calls

--- fre/tests/test_fre_pp_cli.py
+++ fre/tests/test_fre_pp_cli.py
@@ -27,7 +27,7 @@ def test_cli_fre_pp_opt_dne():
 #-- fre pp checkout
 def test_cli_fre_pp_checkout():
     result = runner.invoke(fre.fre, args=["pp", "checkout"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_checkout_help():
     result = runner.invoke(fre.fre, args=["pp", "checkout", "--help"])
@@ -40,7 +40,7 @@ def test_cli_fre_pp_checkout_opt_dne():
 #-- fre pp configure-xml
 def test_cli_fre_pp_configure_xml():
     result = runner.invoke(fre.fre, args=["pp", "configure-xml"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_configure_xml_help():
     result = runner.invoke(fre.fre, args=["pp", "configure-xml", "--help"])
@@ -53,7 +53,7 @@ def test_cli_fre_pp_configure_xml_opt_dne():
 #-- fre pp configure-yaml
 def test_cli_fre_pp_configure_yaml():
     result = runner.invoke(fre.fre, args=["pp", "configure-yaml"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_configure_yaml_help():
     result = runner.invoke(fre.fre, args=["pp", "configure-yaml", "--help"])
@@ -66,7 +66,7 @@ def test_cli_fre_pp_configure_yaml_opt_dne():
 #-- fre pp install
 def test_cli_fre_pp_install():
     result = runner.invoke(fre.fre, args=["pp", "install"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_install_help():
     result = runner.invoke(fre.fre, args=["pp", "install", "--help"])
@@ -79,7 +79,7 @@ def test_cli_fre_pp_install_opt_dne():
 #-- fre pp run
 def test_cli_fre_pp_run():
     result = runner.invoke(fre.fre, args=["pp", "run"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_run_help():
     result = runner.invoke(fre.fre, args=["pp", "run", "--help"])
@@ -92,7 +92,7 @@ def test_cli_fre_pp_run_opt_dne():
 #-- fre pp status 
 def test_cli_fre_pp_status():
     result = runner.invoke(fre.fre, args=["pp", "status"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_status_help():
     result = runner.invoke(fre.fre, args=["pp", "status", "--help"])
@@ -105,7 +105,7 @@ def test_cli_fre_pp_status_opt_dne():
 #-- fre pp validate
 def test_cli_fre_pp_validate():
     result = runner.invoke(fre.fre, args=["pp", "validate"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_validate_help():
     result = runner.invoke(fre.fre, args=["pp", "validate", "--help"])
@@ -118,7 +118,7 @@ def test_cli_fre_pp_validate_opt_dne():
 #-- fre pp wrapper
 def test_cli_fre_pp_wrapper():
     result = runner.invoke(fre.fre, args=["pp", "wrapper"])
-    assert result.exit_code == 0
+    assert result.exit_code == 2
 
 def test_cli_fre_pp_wrapper_help():
     result = runner.invoke(fre.fre, args=["pp", "wrapper", "--help"])
