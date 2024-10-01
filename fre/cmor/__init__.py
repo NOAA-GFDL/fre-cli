0f87dc570fd11ee7e5ed284c558cdc2c49377d83
Ian Laflotte <ilaflott@gmail.com>
2024-10-01 16:37:15 -0400

    remove contents of __init__.py files- none of it is needed for a functional fre-cli and there is no need to complicate the package import structure before we know what we need to change behavior

--- fre/cmor/__init__.py
+++ fre/cmor/__init__.py
@@ -1,5 +1,2 @@
 ''' for fre.cmor imports '''
 from .cmor_mixer import cmor_run_subtool
-from .frecmor import cmor_cli
-
-__all__ = ["cmor_run_subtool", "cmor_cli"]
--- fre/run/__init__.py
+++
@@ -1,6 +0,0 @@
-''' for fre.run imports '''
-
-from .frerunexample import run_test_function
-from .frerun import run_cli
-
-__all__ = ["run_test_function", "run_cli"]
