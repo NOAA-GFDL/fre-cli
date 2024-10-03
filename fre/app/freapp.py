5d20c23430176ab11737236ec1253fc165cf2fc5
Ian Laflotte <ilaflott@gmail.com>
2024-10-03 10:20:55 -0400

    long lines for help strings

--- fre/app/freapp.py
+++ fre/app/freapp.py
@@ -15,37 +15,45 @@ def app_cli():
 
 @app_cli.command()
 @click.option("-i", "--input_dir",
-              type=str,
-              help="`inputDir` / `input_dir` (env var) specifies input directory to regrid, typically an untarredv history file archive",
-              required=True)
+              type = str,
+              help = "`inputDir` / `input_dir` (env var) specifies input directory to regrid, " + \
+                     "typically an untarredv history file archive" ,
+              required = True)
 @click.option("-o", "--output_dir",
-              type=str,
-              help="`outputDir` / `output_dir` (env var) specifies target location for output regridded files",
-              required=True)
+              type = str,
+              help = "`outputDir` / `output_dir` (env var) specifies target location for output" + \
+                     " regridded files",
+              required = True)
 @click.option("-b", "--begin",
-              type=str,
-              help="`begin` / `begin` (env var) ISO8601 datetime format specification for starting date of data, part of input target file name",
-              required=True)
+              type = str,
+              help = "`begin` / `begin` (env var) ISO8601 datetime format specification for" + \
+                     " starting date of data, part of input target file name",
+              required = True)
 @click.option("-t", "--tmp_dir",
-              type=str,
-              help="`TMPDIR` / `tmp_dir` (env var) temp directory for location of file read/writes",
-              required=True)
+              type = str,
+              help = "`TMPDIR` / `tmp_dir` (env var) temp directory for location of file " + \
+                     "read/writes",
+              required = True)
 @click.option("-r", "--remap_dir",
-              type=str,
-              help="`fregridRemapDir` / `remap_dir` (env var) directory containing remap file for regridding",
-              required=True)
+              type = str,
+              help = "`fregridRemapDir` / `remap_dir` (env var) directory containing remap file" + \
+                     " for regridding",
+              required = True)
 @click.option("-s", "--source",
-              type=str,
-              help="`source` / `source` (env var) source name for input target file name within input directory to target for regridding. the value for `source` must be present in at least one component's configuration fields",
-              required=True)
+              type = str,
+              help = "`source` / `source` (env var) source name for input target file name " + \
+                     "within input directory to target for regridding. the value for `source` " + \
+                     "must be present in at least one component's configuration fields",
+              required = True)
 @click.option("-g", "--grid_spec",
-              type=str,
-              help="`gridSpec` / `grid_spec` (env var) file containing mosaic for regridding",
-              required=True)
+              type = str,
+              help = "`gridSpec` / `grid_spec` (env var) file containing mosaic for regridding",
+              required = True)
 @click.option("-I", "--def_xy_interp",
-              type=str,
-              help="`defaultxyInterp` / `def_xy_interp` (env var) default lat/lon resolution for output regridding. (change me? TODO)",
-              required=True)
+              type = str,
+              help = "`defaultxyInterp` / `def_xy_interp` (env var) default lat/lon resolution " + \
+                     "for output regridding. (change me? TODO)",
+              required = True)
 @click.pass_context
 def regrid(context,
               input_dir, output_dir, begin, tmp_dir,
@@ -55,62 +63,62 @@ def regrid(context,
 
 @app_cli.command()
 @click.option("-i", "--infile",
-              type=str,
-              help="Input NetCDF file containing pressure-level output to be masked",
-              required=True)
+              type = str,
+              help = "Input NetCDF file containing pressure-level output to be masked",
+              required = True)
 @click.option("-o", "--outfile",
-              type=str,
-              help="Output file",
-              required=True)
+              type = str,
+              help = "Output file",
+              required = True)
 @click.option("-p", "--psfile",
-              help="Input NetCDF file containing surface pressure (ps)",
-              required=True)
+              help = "Input NetCDF file containing surface pressure (ps)",
+              required = True)
 @click.pass_context
 def mask_atmos_plevel(context, infile, outfile, psfile):
-    # pylint: disable=unused-argument
+    # pylint: disable = unused-argument
     """Mask out pressure level diagnostic output below land surface"""
     context.forward(mask_atmos_plevel_subtool)
 
 
 @app_cli.command()
 @click.option("-i", "--inf",
-              type=str,
-              required=True,
-              help="Input file name")
+              type = str,
+              required = True,
+              help = "Input file name")
 @click.option("-o", "--outf",
-              type=str,
-              required=True,
-              help="Output file name")
+              type = str,
+              required = True,
+              help = "Output file name")
 @click.option("-p", "--pkg",
-              type=click.Choice(["cdo","fre-nctools","fre-python-tools"]),
-              default="cdo",
-              help="Time average approach")
+              type = click.Choice(["cdo","fre-nctools","fre-python-tools"]),
+              default = "cdo",
+              help = "Time average approach")
 @click.option("-v", "--var",
-              type=str,
-              default=None,
-              help="Specify variable to average")
+              type = str,
+              default = None,
+              help = "Specify variable to average")
 @click.option("-u", "--unwgt",
-              is_flag=True,
-              default=False,
-              help="Request unweighted statistics")
+              is_flag = True,
+              default = False,
+              help = "Request unweighted statistics")
 @click.option("-a", "--avg_type",
-              type=click.Choice(["month","seas","all"]),
-              default="all",
-              help="Type of time average to generate. \n \
-                    currently, fre-nctools and fre-python-tools pkg options\n \
-                    do not support seasonal and monthly averaging.\n")
+              type = click.Choice(["month","seas","all"]),
+              default = "all",
+              help = "Type of time average to generate. \n \
+                     currently, fre-nctools and fre-python-tools pkg options\n \
+                     do not support seasonal and monthly averaging.\n")
 @click.option("-s", "--stddev_type",
-              type=click.Choice(["samp","pop","samp_mean","pop_mean"]),
-              default="samp",
-              help="Compute standard deviations for time-averages as well")
+              type = click.Choice(["samp","pop","samp_mean","pop_mean"]),
+              default = "samp",
+              help = "Compute standard deviations for time-averages as well")
 @click.pass_context
 def gen_time_averages(context, inf, outf, pkg, var, unwgt, avg_type, stddev_type):
-    # pylint: disable=unused-argument
+    # pylint: disable = unused-argument
     """
     generate time averages for specified set of netCDF files. 
     Example: generate-time-averages.py /path/to/your/files/
     """
-    start_time=time.perf_counter()
+    start_time = time.perf_counter()
     context.forward(generate)
     # need to change to a click.echo, not sure if echo supports f strings
     print(f'Finished in total time {round(time.perf_counter() - start_time , 2)} second(s)')
