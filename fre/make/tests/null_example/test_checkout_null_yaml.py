2fb2724ad822a71fed4df0ce4f730cd3e3152ccc
Avery Kiihne <Avery.Kiihne@gaea55.ncrc.gov>
2024-10-31 14:53:45 -0400

    change to yaml path #222

--- fre/make/tests/null_example/test_checkout_null_yaml.py
+++ fre/make/tests/null_example/test_checkout_null_yaml.py
@@ -10,27 +10,28 @@ from fre.make import createCheckout
 # Set example yaml paths, input directory
 CWD = Path.cwd()
 test_dir = Path("fre/make/tests/null_example")
-test_yaml = Path(f"null_model.yaml")
+yamlfile = Path("{test_dir}/null_model.yaml")
 
 #set platform and target
 platform = "ncrc5.intel"
 target = "debug"
 yamlfile = "null_model.yaml"
 
+#set output directory
+out_dir = Path(f"{os.getenv('HOME')}/configure_yaml_out/fremake_canopy/test/null_model_full/src")
+Path(out_dir).mkdir(parents=True,exist_ok=True)
+
 # Set home for ~/cylc-src location in script
-os.environ["HOME"]=str(Path(f"{CWD}/{test_dir}/configure_yaml_out"))
+os.environ["HOME"]=str(Path(f"{out_dir}"))
 
 #run checkout command
 runner = CliRunner()
 
-# Set output directory
-out_dir = Path(f"{os.getenv('HOME')}/fremake_canopy/test/null_model_full/src")
-Path(out_dir).mkdir(parents=True,exist_ok=True)
 def test_checkout_script_exists():
     """
     Make sure checkout file exists
     """
-    result = runner.invoke(fre.fre, args=["make","create-checkout","-y","null_model.yaml","-p","ncrc5.intel","-t","debug"])
+    result = runner.invoke(fre.fre, args=["make","create-checkout","-y","{yamlfile}","-p","ncrc5.intel","-t","debug"])
     #createCheckout.checkout_create(["null_model.yaml","ncrc5.intel","debug"])
     assert Path(f"{out_dir}/checkout.sh").exists()
 
@@ -39,6 +40,6 @@ def test_checkout_execute():
     check if --execute option works
     """
     subprocess.run(["rm","-rf",f"{out_dir}"])
-    result = runner.invoke(fre.fre, args=["make","create-checkout","-y","null_model.yaml","-p","ncrc5.intel","-t","debug","--execute"])
+    result = runner.invoke(fre.fre, args=["make","create-checkout","-y","{yamlfile}","-p","ncrc5.intel","-t","debug","--execute"])
     assert (result.exit_code == 0)
 
