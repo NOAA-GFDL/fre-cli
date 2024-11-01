179ce3f68970f940a93f3a56dac150a049f55eae
Dana Singh <Dana.Singh@noaa.gov>
2024-11-01 16:38:17 -0400

    #223 Update `test_create_makefile.py`

--- fre/make/tests/test_create_makefile.py
+++ fre/make/tests/test_create_makefile.py
@@ -13,6 +13,7 @@ YAMLFILE = "null_model.yaml"
 BM_PLATFORM = ["ncrc5.intel23"]
 CONTAINER_PLATFORM = ["hpcme.2023"]
 TARGET = ["debug"]
+EXPERIMENT = "null_model_full"
 
 # Create output location
 out = f"{test_dir}/makefile_out"
@@ -49,12 +50,19 @@ def test_bm_makefile_creation():
     """
     Check the makefile is created when a bare-metal platform is used
     """
+    bm_plat = BM_PLATFORM[0]
+    targ = TARGET[0]
     yamlfile_path = f"{test_dir}/{NM_EXAMPLE}/{YAMLFILE}"
     createMakefile.makefile_create(yamlfile_path,BM_PLATFORM,TARGET)
+
+    assert Path(f"{out}/fremake_canopy/test/{EXPERIMENT}/{bm_plat}-{targ}/exec/Makefile").exists()
 
 def test_container_makefile_creation():
     """
     Check the makefile is created when the container platform is used
     """
+    container_plat = CONTAINER_PLATFORM[0]
     yamlfile_path = f"{test_dir}/{NM_EXAMPLE}/{YAMLFILE}"
     createMakefile.makefile_create(yamlfile_path,CONTAINER_PLATFORM,TARGET)
+
+    assert Path(f"tmp/{container_plat}/Makefile").exists()
