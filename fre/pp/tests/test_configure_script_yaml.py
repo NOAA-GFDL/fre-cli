6e3307ba312309d9cba405e404c6549e5b0a7082
Dana Singh <Dana.Singh@noaa.gov>
2024-11-04 13:58:21 -0500

    #223 Apply pylint messages

--- fre/pp/tests/test_configure_script_yaml.py
+++ fre/pp/tests/test_configure_script_yaml.py
@@ -1,15 +1,18 @@
+"""
+Test configure_script_yaml
+"""
 import os
 from pathlib import Path
 from fre.pp import configure_script_yaml as csy
 
 # Set what would be click options
-experiment = "c96L65_am5f7b12r1_amip"
-platform = "gfdl.ncrc5-intel22-classic"
-target = "prod-openmp"
+EXPERIMENT = "c96L65_am5f7b12r1_amip"
+PLATFORM = "gfdl.ncrc5-intel22-classic"
+TARGET = "prod-openmp"
 
 # Set example yaml paths, input directory
 test_dir = Path("fre/pp/tests")
-test_yaml = Path(f"AM5_example/am5.yaml")
+test_yaml = Path("AM5_example/am5.yaml")
 
 def test_combinedyaml_exists():
     """
@@ -27,17 +30,17 @@ def test_configure_script():
     os.environ["HOME"]=str(Path(f"{test_dir}/configure_yaml_out"))
 
     # Set output directory
-    out_dir = Path(f"{os.getenv('HOME')}/cylc-src/{experiment}__{platform}__{target}")
+    out_dir = Path(f"{os.getenv('HOME')}/cylc-src/{EXPERIMENT}__{PLATFORM}__{TARGET}")
     Path(out_dir).mkdir(parents=True,exist_ok=True)
 
     # Define combined yaml
     model_yaml = str(Path(f"{test_dir}/{test_yaml}"))
 
     # Invoke configure_yaml_script.py
-    csy._yamlInfo(model_yaml,experiment,platform,target)
+    csy.yamlInfo(model_yaml,EXPERIMENT,PLATFORM,TARGET)
 
     # Check for configuration creation and final combined yaml
-    assert all([Path(f"{out_dir}/{experiment}.yaml").exists(),
+    assert all([Path(f"{out_dir}/{EXPERIMENT}.yaml").exists(),
                 Path(f"{out_dir}/rose-suite.conf").exists(),
                 Path(f"{out_dir}/app/regrid-xy/rose-app.conf").exists(),
                 Path(f"{out_dir}/app/remap-pp-components/rose-app.conf").exists()])
