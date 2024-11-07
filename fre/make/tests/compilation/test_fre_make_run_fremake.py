29795a9ac7b072782418a18489f42f7a7cfb907f
rem1776 <Ryan.Mulhall@noaa.gov>
2024-11-07 14:50:35 -0500

    update run-fremake test file to use a direct function call and check executable exists

--- fre/make/tests/compilation/test_fre_make_run_fremake.py
+++ fre/make/tests/compilation/test_fre_make_run_fremake.py
@@ -1,13 +1,21 @@
-''' test "fre make" calls '''
+''' test "fre make run-fremake" calls '''
 
-from click.testing import CliRunner
+import os
+from fre.make import runFremake
+from pathlib import Path
 
-from fre import fre
+# command options
+YAMLFILE = "fre/make/tests/null_example/null_model.yaml"
+PLATFORM = [ "ci.gnu" ]
+CONTAINER_PLATFORM = ["hpcme.2023"]
+TARGET = ["debug"]
+EXPERIMENT = "null_model_full"
 
-runner = CliRunner()
+# get HOME dir to check output
+HOME_DIR = os.environ["HOME"]
 
-## assumes you're running pytest from repo root dir
-def test_fre_make_run_fremake_null_model():
+def test_fre_make_run_fremake_null_model_serial_compile():
     ''' run fre make with run-fremake subcommand and build the null model experiment with gnu'''
-    result = runner.invoke(fre.fre, args=["make", "run-fremake", "-y", "fre/make/tests/null_example/null_model.yaml", "-p", "ci.gnu", "-t", "debug"])
-    assert result.exit_code == 0
+    runFremake.fremake_run(YAMLFILE, PLATFORM, TARGET, False, 1, False, False)
+    assert Path(f"{HOME_DIR}/fremake_canopy/test/{EXPERIMENT}/{PLATFORM[0]}-{TARGET[0]}/exec/{EXPERIMENT}.x").exists()
+
