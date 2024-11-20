670cf66f29a752636bb4bbc290e4d2098ed6fe5f
Ryan Mulhall <35538242+rem1776@users.noreply.github.com>
2024-11-20 14:50:15 -0500

    add option to test_fre_make_run_fremake.py

--- fre/make/tests/compilation/test_fre_make_run_fremake.py
+++ fre/make/tests/compilation/test_fre_make_run_fremake.py
@@ -16,6 +16,6 @@ HOME_DIR = os.environ["HOME"]
 
 def test_fre_make_run_fremake_null_model_serial_compile():
     ''' run fre make with run-fremake subcommand and build the null model experiment with gnu'''
-    runFremake.fremake_run(YAMLFILE, PLATFORM, TARGET, False, 1, False, False)
+    runFremake.fremake_run(YAMLFILE, PLATFORM, TARGET, False, 1, False, True, False)
     assert Path(f"{HOME_DIR}/fremake_canopy/test/{EXPERIMENT}/{PLATFORM[0]}-{TARGET[0]}/exec/{EXPERIMENT}.x").exists()
 
