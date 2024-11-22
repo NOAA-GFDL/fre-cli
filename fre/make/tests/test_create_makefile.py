ece0d33f7fbdbc2fb6cf88a2df67237584d826ac
Ian Laflotte <ilaflott@gmail.com>
2024-11-22 16:33:01 -0500

    tweak makfile test to restore homedir to what it once was before the test runs. for later checkout test success.

--- fre/make/tests/test_create_makefile.py
+++ fre/make/tests/test_create_makefile.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from fre.make import createMakefile
 
 # SET-UP
-test_dir = Path("fre/make/tests")
+TEST_DIR = Path("fre/make/tests")
 NM_EXAMPLE = Path("null_example")
 YAMLFILE = "null_model.yaml"
 BM_PLATFORM = ["ncrc5.intel23"]
@@ -16,35 +16,35 @@ TARGET = ["debug"]
 EXPERIMENT = "null_model_full"
 
 # Create output location
-out = f"{test_dir}/makefile_out"
-if Path(out).exists():
+OUT = f"{TEST_DIR}/makefile_out"
+if Path(OUT).exists():
     # remove
-    shutil.rmtree(out)
+    shutil.rmtree(OUT)
     # create output directory
-    Path(out).mkdir(parents=True,exist_ok=True)
+    Path(OUT).mkdir(parents=True,exist_ok=True)
 else:
-    Path(out).mkdir(parents=True,exist_ok=True)
+    Path(OUT).mkdir(parents=True,exist_ok=True)
 
 # Set output directory as home for fre make output
-#os.environ["HOME"]=str(Path(out))
+#os.environ["HOME"]=str(Path(OUT))
 
 def test_modelyaml_exists():
     """
     Check the model yaml exists
     """
-    assert Path(f"{test_dir}/{NM_EXAMPLE}/{YAMLFILE}").exists()
+    assert Path(f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}").exists()
 
 def test_compileyaml_exists():
     """
     Check the compile yaml exists
     """
-    assert Path(f"{test_dir}/{NM_EXAMPLE}/compile.yaml").exists()
+    assert Path(f"{TEST_DIR}/{NM_EXAMPLE}/compile.yaml").exists()
 
 def test_platformyaml_exists():
     """
     Check the platform yaml exists
     """
-    assert Path(f"{test_dir}/{NM_EXAMPLE}/platforms.yaml").exists()
+    assert Path(f"{TEST_DIR}/{NM_EXAMPLE}/platforms.yaml").exists()
 
 def test_bm_makefile_creation():
     """
@@ -52,16 +52,16 @@ def test_bm_makefile_creation():
     """
     # Set output directory as home for fre make output
     def_home = str(os.environ["HOME"])
-    os.environ["HOME"]=out#str(Path(out))
+    os.environ["HOME"]=OUT#str(Path(OUT))
 
     bm_plat = BM_PLATFORM[0]
     targ = TARGET[0]
-    yamlfile_path = f"{test_dir}/{NM_EXAMPLE}/{YAMLFILE}"
+    yamlfile_path = f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}"
 
     createMakefile.makefile_create(yamlfile_path,BM_PLATFORM,TARGET)
 
-    assert Path(f"{out}/fremake_canopy/test/{EXPERIMENT}/{bm_plat}-{targ}/exec/Makefile").exists()
-    os.environ["HOME"]=def_home
+    assert Path(f"{OUT}/fremake_canopy/test/{EXPERIMENT}/{bm_plat}-{targ}/exec/Makefile").exists()
+    os.environ["HOME"] = def_home
     assert os.environ["HOME"] == def_home
 
 def test_container_makefile_creation():
@@ -69,7 +69,7 @@ def test_container_makefile_creation():
     Check the makefile is created when the container platform is used
     """
     container_plat = CONTAINER_PLATFORM[0]
-    yamlfile_path = f"{test_dir}/{NM_EXAMPLE}/{YAMLFILE}"
+    yamlfile_path = f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}"
     createMakefile.makefile_create(yamlfile_path,CONTAINER_PLATFORM,TARGET)
 
     assert Path(f"tmp/{container_plat}/Makefile").exists()
