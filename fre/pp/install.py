de6c8c7a811a8b62c64fa9a3949b8cc21c612dba
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2024-11-25 22:26:25 -0500

    logic for fre pp install to compare configurations

--- fre/pp/install.py
+++ fre/pp/install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 ''' fre pp install '''
 
+from pathlib import Path
+import os
 import subprocess
 import click
 
@@ -13,8 +15,26 @@ def _install_subtool(experiment, platform, target):
     """
 
     name = experiment + '__' + platform + '__' + target
-    cmd = f"cylc install --no-run-name {name}"
-    subprocess.run(cmd, shell=True, check=True)
+    # if the cylc-run directory already exists,
+    # then check whether the cylc expanded definition (cylc config)
+    # is identical. If the same, good. If not, bad.
+    source_dir = Path(os.path.expanduser("~/cylc-src"), name)
+    install_dir = Path(os.path.expanduser("~/cylc-run"), name)
+    if os.path.isdir(install_dir):
+        installed_def = subprocess.run(["cylc", "config", name],capture_output=True).stdout
+        go_back_here = os.getcwd()
+        os.chdir(source_dir)
+        source_def = subprocess.run(['cylc', 'config', '.'], capture_output=True).stdout
+        if installed_def == source_def:
+            print(f"NOTE: Workflow '{install_dir}' already installed, and the definition is unchanged")
+        else:
+            print(f"ERROR: Workflow '{install_dir}' already installed, and the definition has changed!")
+            print(f"ERROR: Please manually remove or archive your workflow run directory '{install_dir}' and then run again")
+            raise Exception
+    else:
+        print(f"NOTE: About to install workflow into ~/cylc-run/{name}")
+        cmd = f"cylc install --no-run-name {name}"
+        subprocess.run(cmd, shell=True, check=True)
 
 @click.command()
 def install_subtool(experiment, platform, target):
