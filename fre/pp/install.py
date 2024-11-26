c42687bcf44f1f78254b1ac88d3aeafdf01a4c5f
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2024-11-25 23:55:42 -0500

    Somewhat improved error message

--- fre/pp/install.py
+++ fre/pp/install.py
@@ -29,7 +29,7 @@ def install_subtool(experiment, platform, target):
             print(f"NOTE: Workflow '{install_dir}' already installed, and the definition is unchanged")
         else:
             print(f"ERROR: Workflow '{install_dir}' already installed, and the definition has changed!")
-            print(f"ERROR: Please manually remove or archive your workflow run directory '{install_dir}' and then run again")
+            print(f"ERROR: Please remove installed workflow with 'cylc clean {name}' or move the workflow run directory '{install_dir}'")
             exit(1)
     else:
         print(f"NOTE: About to install workflow into ~/cylc-run/{name}")
