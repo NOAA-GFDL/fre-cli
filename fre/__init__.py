6a146a3287ab912036dae2d4ce2c89fa4af94ed1
Ian Laflotte <ilaflott@gmail.com>
2024-11-27 13:30:28 -0500

    versioning is not fun

--- fre/__init__.py
+++ fre/__init__.py
@@ -11,5 +11,5 @@ if len(version_unexpanded_split[1]) == 1:
     version_minor = "0" + version_unexpanded_split[1]
 else:
     version_minor = version_unexpanded_split[1]
-version = version_unexpanded_split[0] + '.' + version_minor
-__version__ = version
+__version__ = version_unexpanded_split[0] + '.' + version_minor
+
