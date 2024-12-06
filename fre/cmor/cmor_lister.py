237402e3588fc0407e70a9e031bf0529eec6f9c9
Ian Laflotte <ilaflott@gmail.com>
2024-12-06 14:15:07 -0500

    cmor lister updates, largely clean up of stdout text

--- fre/cmor/cmor_lister.py
+++ fre/cmor/cmor_lister.py
@@ -19,8 +19,12 @@ from pathlib import Path
 
 import click
 
-def print_var_content( table_config_file = None, opt_var_name = None):
-    ''' one variable printing routine '''
+DO_NOT_PRINT_LIST=[ 'comment',
+                    'ok_min_mean_abs', 'ok_max_mean_abs',
+                    'valid_min', 'valid_max' ]
+
+def print_var_content( table_config_file = None, var_name = None):
+    ''' one variable printing routine- looks for info regarding var_name in table_config_file '''
     try:
         proj_table_vars=json.load(table_config_file)
     except Exception as exc:
@@ -28,30 +32,35 @@ def print_var_content( table_config_file = None, opt_var_name = None):
         
     var_content = None
     try:
-        var_content = proj_table_vars["variable_entry"].get(opt_var_name)
+        var_content = proj_table_vars["variable_entry"].get(var_name)
     except:
         #print(f'(cmor_list_subtool) WARNING no "variable_entry" key. for {json_table_config}.'
         #       '                    not the right json file probably. moving on!')
         return
 
     if var_content is None:
-        #print(f'(cmor_list_subtool) variable {opt_var_name} not found in {Path(json_table_config).name}, moving on!')
+        #print(f'(cmor_list_subtool) variable {var_name} not found in {Path(json_table_config).name}, moving on!')
         return
     
     table_name = None
     try:
-        print(f'(print_var_content) trying to get table_name from proj_table_vars...')
-        table_name = proj_table_vars["Header"].get(table_name)
+        #print(f'(print_var_content) trying to get table_name from proj_table_vars...')
+        #print(f'                    table header is {proj_table_vars["Header"]}')
+        table_name = proj_table_vars["Header"].get('table_id').split(' ')[1]
+        #print(f'                    table_name = {table_name}')
     except:
         print(f'print_var_content) WARNING couldnt get header and table_name field')
         pass
 
-    print(f'(print_var_content) found {opt_var_name} data in table!\n')
     if table_name is not None:
-        print(f'           table_name = {table_name}')
-        
-    print(f'                    {opt_var_name} stuff is = \n')
-    for content in var_content:        
+        print(f'(print_var_content) found {var_name} data in table {table_name}!')
+    else:
+        print(f'(print_var_content) found {var_name} data in table, but not its table_name!')
+
+    print(f'              variable key: {var_name}')
+    for content in var_content:
+        if content in DO_NOT_PRINT_LIST:
+            continue
         print(f'              {content}: {var_content[content]}')        
     print('\n')
     
@@ -80,13 +89,15 @@ def cmor_list_subtool( json_var_list = None, json_table_config_dir = None, opt_v
     if opt_var_name is None and var_list is None:
         raise ValueError(f'(cmor_list_subtool) ERROR: no opt_var_name given but also no content in variable list!!! exit!')
 
-    if opt_var_name is not None:    
+    if opt_var_name is not None:
+        print(f'(cmor_list_subtool) opt_var_name is not None: looking for only ONE variables worth of info!')
         for json_table_config in json_table_configs:
-            print(f'(cmor_list_subtool) attempting to open {json_table_config}')        
+            #print(f'(cmor_list_subtool) attempting to open {json_table_config}')        
             with open( json_table_config, "r", encoding = "utf-8") as table_config_file:
                 print_var_content(table_config_file, opt_var_name)
             
     elif var_list is not None:
+        print(f'(cmor_list_subtool) opt_var_name is None, and var_list is not None, looking for many variables worth of info!')
         for var in var_list:            
             for json_table_config in json_table_configs:
                 #print(f'(cmor_list_subtool) attempting to open {json_table_config}')        
