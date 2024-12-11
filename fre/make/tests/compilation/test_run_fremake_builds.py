cdc2ca66bf93a2f499da369ed28945727ccc59f7
rem1776 <Ryan.Mulhall@noaa.gov>
2024-12-11 13:16:57 -0500

    update compilation tests

--- fre/make/tests/compilation/test_run_fremake_builds.py
+++ fre/make/tests/compilation/test_run_fremake_builds.py
@@ -1,5 +1,6 @@
-''' test "fre make run-fremake" calls '''
+''' this test file holds any run-fremake tests that actually compile the model code'''
 ''' these tests assume your os is the ci image (gcc 14 + mpich on rocky 8)'''
+''' you may need to add mkmf to your path or make other adjustments to the mkmf template to run elsewhere'''
 
 import os
 from shutil  import rmtree
@@ -30,22 +31,22 @@ Path(MULTIJOB_TEST_PATH).mkdir(parents=True,exist_ok=True)
 
 
 # test building the null model using gnu compilers
-def test_fre_make_run_fremake_serial_compile():
+def test_run_fremake_serial_compile():
     ''' run fre make with run-fremake subcommand and build the null model experiment with gnu'''
     os.environ["TEST_BUILD_DIR"] = SERIAL_TEST_PATH
     run_fremake_script.fremake_run(YAMLPATH, PLATFORM, TARGET, False, 1, False, True, VERBOSE)
     assert Path(f"{SERIAL_TEST_PATH}/fremake_canopy/test/{EXPERIMENT}/{PLATFORM[0]}-{TARGET[0]}/exec/{EXPERIMENT}.x").exists()
 
-def test_fre_make_run_fremake_multijob_compile():
+# same test with a parallel build
+def test_run_fremake_multijob_compile():
     ''' test run-fremake parallel compile with gnu'''
     os.environ["TEST_BUILD_DIR"] = MULTIJOB_TEST_PATH
     run_fremake_script.fremake_run(YAMLPATH, PLATFORM, TARGET, True, 4, False, True, VERBOSE)
     assert Path(f"{MULTIJOB_TEST_PATH}/fremake_canopy/test/{EXPERIMENT}/{PLATFORM[0]}-{TARGET[0]}/exec/{EXPERIMENT}.x").exists()
 
-
-# dockerfile tests
-
-#def test_fre_make_run_fremake_dockerfile_creation():
-#    ''' checks dockerfile creation for the container build'''
-#    run_fremake_script.fremake_run(YAMLPATH, CONTAINER_PLATFORM, TARGET, False, 1, False, False, VERBOSE)
+# containerized build
+def test_run_fremake_dockerfile_creation():
+    ''' checks dockerfile creation for the container build'''
+    run_fremake_script.fremake_run(YAMLPATH, CONTAINER_PLATFORM, TARGET, False, 1, True, True, VERBOSE)
+    assert Path("null_model_full-debug.sif").exists()
 
