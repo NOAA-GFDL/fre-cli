b058aa55d6bc8cfb40b017b7b0969f99aa23aefc
kiihne-noaa <avery.kiihne@noaa.gov>
2025-01-27 14:58:32 -0500

    retrying directory

--- fre/make/tests/null_example/test_checkout_null_yaml.py
+++ fre/make/tests/null_example/test_checkout_null_yaml.py
@@ -15,7 +15,7 @@ TARGET = ["debug"]
 #set output directory
 # Set home for ~/cylc-src location in script
 #run checkout command
-OUT = f"{TEST_DIR}/checkout_out"
+OUT = f"{TEST_DIR}/configure_yaml_out"
 os.environ["TEST_BUILD_DIR"] = OUT
 
 def test_checkout_script_exists():
