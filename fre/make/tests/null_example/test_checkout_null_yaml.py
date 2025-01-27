17b609fd0ad33d395fcb6e70ed0b8e94ebe557aa
kiihne-noaa <avery.kiihne@noaa.gov>
2025-01-27 14:35:32 -0500

    adressing various comments

--- fre/make/tests/null_example/test_checkout_null_yaml.py
+++ fre/make/tests/null_example/test_checkout_null_yaml.py
@@ -9,13 +9,13 @@ TEST_DIR = str(Path("fre/make/tests"))
 YAMLFILE = str(Path(f"{TEST_DIR}/null_example/null_model.yaml"))
 
 #set platform and target
-PLATFORM = ["ncrc5.intel23"]
+PLATFORM = ["ci.gnu"]
 TARGET = ["debug"]
 
 #set output directory
 # Set home for ~/cylc-src location in script
 #run checkout command
-OUT = f"{TEST_DIR}/configure_yaml_out"
+OUT = f"{TEST_DIR}/checkout_out"
 os.environ["TEST_BUILD_DIR"] = OUT
 
 def test_checkout_script_exists():
@@ -23,7 +23,12 @@ def test_checkout_script_exists():
     Make sure checkout file exists
     """
     subprocess.run(["rm","-rf",f"{OUT}/fremake_canopy/test/null_model_full/src/checkout.sh"])
-    create_checkout_script.checkout_create(YAMLFILE,PLATFORM,TARGET,False,False, False, False)
+    create_checkout_script.checkout_create(YAMLFILE,
+                                           PLATFORM,
+                                           TARGET,
+                                           no_parallel_checkout = False,
+                                           jobs = False, execute = False,
+                                           verbose = False)
     #assert result.exit_code == 0
     assert Path(f"{OUT}/fremake_canopy/test/null_model_full/src/checkout.sh").exists()
 
@@ -31,14 +36,26 @@ def test_checkout_verbose():
     """
     check if --verbose option works
     """
-    create_checkout_script.checkout_create(YAMLFILE,PLATFORM,TARGET,False,False, False,True)
+    create_checkout_script.checkout_create(YAMLFILE,
+                                           PLATFORM,
+                                           TARGET, 
+                                           no_parallel_checkout = False,
+                                           jobs = False, 
+                                           execute = False,
+                                           verbose = True)
 
 def test_checkout_execute():
     """
     check if --execute option works
     """
     subprocess.run(["rm","-rf",f"{OUT}/fremake_canopy/test"])
-    create_checkout_script.checkout_create(YAMLFILE,PLATFORM,TARGET,False,2, True,False)
+    create_checkout_script.checkout_create(YAMLFILE,
+                                           PLATFORM,
+                                           TARGET,
+                                           no_parallel_checkout = False,
+                                           jobs = 2,
+                                           execute = True,
+                                           verbose = False)
     
 def test_checkout_no_parallel_checkout():
     """
