320e27052f63dbbecf5d9cdefec750e25b5f4c7e
kiihne-noaa <avery.kiihne@noaa.gov>
2025-01-27 09:29:37 -0500

    running execute with jobs num designated

--- fre/make/tests/null_example/test_checkout_null_yaml.py
+++ fre/make/tests/null_example/test_checkout_null_yaml.py
@@ -38,4 +38,4 @@ def test_checkout_execute():
     check if --execute option works
     """
     subprocess.run(["rm","-rf",f"{OUT}/fremake_canopy/test"])
-    create_checkout_script.checkout_create(YAMLFILE,PLATFORM,TARGET,False,False, True,False)
+    create_checkout_script.checkout_create(YAMLFILE,PLATFORM,TARGET,False,2, True,False)
