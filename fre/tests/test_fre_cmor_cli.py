78767d66078e8624935833dc16cce3c4ddbd3d6a
Ian Laflotte <ilaflott@gmail.com>
2025-01-31 14:22:33 -0500

    proper unit test- plugged in. naive yaml case works fine.

--- fre/tests/test_fre_cmor_cli.py
+++ fre/tests/test_fre_cmor_cli.py
@@ -14,7 +14,31 @@ import subprocess
 runner = CliRunner()
 
 # where are we? we're running pytest from the base directory of this repo
-rootdir = 'fre/tests/test_files'
+ROOTDIR = 'fre/tests/test_files'
+
+# these unit tests should be more about the cli, rather than the workload
+YYYYMMDD=date.today().strftime('%Y%m%d')
+
+copied_nc_filename = 'reduced_ocean_monthly_1x1deg.199307-199308.sosV2.nc'
+full_copied_nc_filepath = f'{ROOTDIR}/ocean_sos_var_file/{copied_nc_filename}'
+original_nc_file = f'{ROOTDIR}/ocean_sos_var_file/reduced_ocean_monthly_1x1deg.199307-199308.sos.nc'
+
+def test_setup_test_files(capfd):
+    "set-up test: copy and rename NetCDF file created in test_fre_cmor_run_subtool.py"
+
+    assert Path(original_nc_file).exists()
+
+    if Path(full_copied_nc_filepath).exists():
+        Path(full_copied_nc_filepath).unlink()
+    assert not Path(full_copied_nc_filepath).exists()
+
+    shutil.copy(Path(original_nc_file), Path(full_copied_nc_filepath))
+
+    assert (Path(full_copied_nc_filepath).exists())
+
+    out, err = capfd.readouterr()
+
+
 
 # fre cmor
 def test_cli_fre_cmor():
@@ -50,8 +74,29 @@ def test_cli_fre_cmor_yaml_opt_dne():
 
 def test_cli_fre_cmor_yaml_case1():
     ''' fre cmor yaml -y '''
-    result = runner.invoke(fre.fre, args=["cmor", "yaml", "-y", f"{rootdir}/cmor.yaml"])
-    assert result.exit_code == 0
+    
+    # FYI
+    indir = f'{ROOTDIR}/ocean_sos_var_file'
+    filename = 'reduced_ocean_monthly_1x1deg.199307-199308.sos.nc' # unneeded, this is mostly for reference
+    full_inputfile=f"{indir}/{filename}"
+    
+    # determined by cmor_run_subtool
+    outdir = f'{ROOTDIR}/outdir'
+    cmor_creates_dir = \
+        'CMIP6/CMIP6/ISMIP6/PCMDI/PCMDI-test-1-0/piControl-withism/r3i1p1f1/Omon/sos/gn'
+    full_outputdir = \
+        f"{outdir}/{cmor_creates_dir}/v{YYYYMMDD}" # yay no more 'fre' where it shouldnt be
+    full_outputfile = \
+        f"{full_outputdir}/sos_Omon_PCMDI-test-1-0_piControl-withism_r3i1p1f1_gn_199307-199308.nc"
+
+
+    result = runner.invoke(fre.fre, args=["cmor", "yaml", "-y", f"{ROOTDIR}/cmor.yaml"])
+
+
+    assert all ( [ result.exit_code == 0,
+                   Path(full_outputfile).exists(),
+                   Path(full_inputfile).exists() ] )
+
 
 # fre cmor run
 def test_cli_fre_cmor_run():
@@ -69,36 +114,15 @@ def test_cli_fre_cmor_run_opt_dne():
     result = runner.invoke(fre.fre, args=["cmor", "run", "optionDNE"])
     assert result.exit_code == 2
 
-copied_nc_filename = 'reduced_ocean_monthly_1x1deg.199307-199308.sosV2.nc'
-full_copied_nc_filepath = f'{rootdir}/ocean_sos_var_file/{copied_nc_filename}'
-original_nc_file = f'{rootdir}/ocean_sos_var_file/reduced_ocean_monthly_1x1deg.199307-199308.sos.nc'
-
-def test_setup_test_files(capfd):
-    "set-up test: copy and rename NetCDF file created in test_fre_cmor_run_subtool.py"
-
-    assert Path(original_nc_file).exists()
-
-    if Path(full_copied_nc_filepath).exists():
-        Path(full_copied_nc_filepath).unlink()
-    assert not Path(full_copied_nc_filepath).exists()
-
-    shutil.copy(Path(original_nc_file), Path(full_copied_nc_filepath))
-
-    assert (Path(full_copied_nc_filepath).exists())
-
-    out, err = capfd.readouterr()
-
-# these unit tests should be more about the cli, rather than the workload
-YYYYMMDD=date.today().strftime('%Y%m%d')
 def test_cli_fre_cmor_run_case1(capfd):
     ''' fre cmor run, test-use case '''
 
     # explicit inputs to tool
-    indir = f'{rootdir}/ocean_sos_var_file/'
-    varlist = f'{rootdir}/varlist'
-    table_config = f'{rootdir}/cmip6-cmor-tables/Tables/CMIP6_Omon.json'
-    exp_config = f'{rootdir}/CMOR_input_example.json'
-    outdir = f'{rootdir}/outdir'
+    indir = f'{ROOTDIR}/ocean_sos_var_file/'
+    varlist = f'{ROOTDIR}/varlist'
+    table_config = f'{ROOTDIR}/cmip6-cmor-tables/Tables/CMIP6_Omon.json'
+    exp_config = f'{ROOTDIR}/CMOR_input_example.json'
+    outdir = f'{ROOTDIR}/outdir'
 
     # determined by cmor_run_subtool
     cmor_creates_dir = \
@@ -132,16 +156,13 @@ def test_cli_fre_cmor_run_case1(capfd):
 
 def test_cli_fre_cmor_run_case2(capfd):
     ''' fre cmor run, test-use case '''
-
-    # where are we? we're running pytest from the base directory of this repo
-    rootdir = 'fre/tests/test_files'
-
+    
     # explicit inputs to tool
-    indir = f'{rootdir}/ocean_sos_var_file'
-    varlist = f'{rootdir}/varlist_local_target_vars_differ'
-    table_config = f'{rootdir}/cmip6-cmor-tables/Tables/CMIP6_Omon.json'
-    exp_config = f'{rootdir}/CMOR_input_example.json'
-    outdir = f'{rootdir}/outdir'
+    indir = f'{ROOTDIR}/ocean_sos_var_file'
+    varlist = f'{ROOTDIR}/varlist_local_target_vars_differ'
+    table_config = f'{ROOTDIR}/cmip6-cmor-tables/Tables/CMIP6_Omon.json'
+    exp_config = f'{ROOTDIR}/CMOR_input_example.json'
+    outdir = f'{ROOTDIR}/outdir'
 
     # determined by cmor_run_subtool
     cmor_creates_dir = \
