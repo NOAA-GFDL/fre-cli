768e2c6d66d80324c80167f583834abd8b159cd9
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2025-02-03 12:17:39 -0500

    #221 Add `force-dockerfile` functionality

--- fre/make/create_docker_script.py
+++ fre/make/create_docker_script.py
@@ -6,11 +6,39 @@ import os
 import sys
 import subprocess
 from pathlib import Path
-
 from .gfdlfremake import varsfre, targetfre, yamlfre, buildDocker
 import fre.yamltools.combine_yamls as cy
 
-def dockerfile_create(yamlfile, platform, target, execute):
+def dockerfile_write_steps(yaml_obj,img,run_env,target,mkTemplate,s2i,td,cr,cb,cd):
+    """
+    Go through steps to write the Dockerfile
+    """
+    dockerBuild = buildDocker.container(base = img,
+                                        exp = yaml_obj["experiment"],
+                                        libs = yaml_obj["container_addlibs"],
+                                        RUNenv = run_env,
+                                        target = target,
+                                        mkTemplate = mkTemplate,
+                                        stage2base = s2i)
+
+    dockerBuild.writeDockerfileCheckout("checkout.sh", td+"/checkout.sh")
+    dockerBuild.writeDockerfileMakefile(td+"/Makefile", td+"/linkline.sh")
+
+    for c in yaml_obj['src']:
+        dockerBuild.writeDockerfileMkmf(c)
+
+    dockerBuild.writeRunscript(run_env,cr,td+"/execrunscript.sh")
+    print(f"    Container runscript created here: ./{td}/execrunscript.sh")
+    print(f"    Dockerfile created here: {cd}/Dockerfile")
+
+    # Create build script for container
+    dockerBuild.createBuildScript(cb, cr)
+    print(f"    Container build script created here: {dockerBuild.userScriptPath}\n")
+
+def dockerfile_create(yamlfile,platform,target,execute,force_dockerfile):
+    """
+    Create the Dockerfile
+    """
     srcDir="src"
     checkoutScriptName = "checkout.sh"
     baremetalRun = False # This is needed if there are no bare metal runs
@@ -19,7 +47,6 @@ def dockerfile_create(yamlfile, platform, target, execute):
     tlist = target
     yml = yamlfile
     name = yamlfile.split(".")[0]
-    run = execute
 
     # Combined compile yaml file
     combined = Path(f"combined-{name}.yaml")
@@ -53,33 +80,49 @@ def dockerfile_create(yamlfile, platform, target, execute):
             if platform["container"] is True:
                 image=modelYaml.platforms.getContainerImage(platformName)
                 stage2image = modelYaml.platforms.getContainer2base(platformName)
-                bldDir = platform["modelRoot"] + "/" + fremakeYaml["experiment"] + "/exec"
-                tmpDir = "tmp/"+platformName
-                dockerBuild = buildDocker.container(base = image,
-                                              exp = fremakeYaml["experiment"],
-                                              libs = fremakeYaml["container_addlibs"],
-                                              RUNenv = platform["RUNenv"],
-                                              target = targetObject,
-                                              mkTemplate = platform["mkTemplate"],
-                                              stage2base = stage2image)
-                dockerBuild.writeDockerfileCheckout("checkout.sh", tmpDir+"/checkout.sh")
-                dockerBuild.writeDockerfileMakefile(tmpDir+"/Makefile", tmpDir+"/linkline.sh")
+                bld_dir = platform["modelRoot"] + "/" + fremakeYaml["experiment"] + "/exec"
+                tmp_dir = "tmp/"+platformName
+                curr_dir = os.getcwd()
+                if not os.path.exists(f"{curr_dir}/Dockerfile"):
+                    dockerfile_write_steps(yaml_obj = fremakeYaml,
+                                           img = image,
+                                           run_env = platform["RUNenv"],
+                                           target = targetObject,
+                                           mkTemplate = platform["mkTemplate"],
+                                           s2i = stage2image,
+                                           td = tmp_dir,
+                                           cr = platform["containerRun"],
+                                           cb = platform["containerBuild"],
+                                           cd = curr_dir)
+                else:
+                    if force_dockerfile:
+                        # Remove the dockerfile
+                        print("\nRemoving previously made dockerfile")
+                        os.remove(curr_dir+"/Dockerfile")
 
-                for c in fremakeYaml['src']:
-                    dockerBuild.writeDockerfileMkmf(c)
+                        # Create the checkout script
+                        print("Re-creating Dockerfile...")
+                        dockerfile_write_steps(yaml_obj = fremakeYaml,
+                                               img = image,
+                                               run_env = platform["RUNenv"],
+                                               target = targetObject,
+                                               mkTemplate = platform["mkTemplate"],
+                                               s2i = stage2image,
+                                               td = tmp_dir,
+                                               cr = platform["containerRun"],
+                                               cb = platform["containerBuild"],
+                                               cd = curr_dir)
+                    else:
+                        print(f"Dockerfile PREVIOUSLY created here: {curr_dir}/Dockerfile")
+                        print(f"Container build script created here: {curr_dir}/createContainer.sh\n")
 
-                dockerBuild.writeRunscript(platform["RUNenv"],platform["containerRun"],tmpDir+"/execrunscript.sh")
-                currDir = os.getcwd()
-
-                # create build script for container
-                dockerBuild.createBuildScript(platform["containerBuild"], platform["containerRun"])
-                print("\ntmpDir created in " + currDir + "/tmp") #was click.echo and a few lines above
-                print("Dockerfile created in " + currDir +"\n") #was click.echo and a few lines above
-                print("Container build script created at "+dockerBuild.userScriptPath+"\n\n")
-
-                # run the script if option is given
-                if run:
-                    subprocess.run(args=[dockerBuild.userScriptPath], check=True)
+                # Execute if flag is given
+                if execute:
+                    try:
+                        subprocess.run(args=[f"{curr_dir}/createContainer.sh"], check=True)
+                    except:
+                        print(f"There was an error in runnning the container build script: {curr_dir}/createContainer.sh")
+                        raise
 
 if __name__ == "__main__":
     dockerfile_create()
