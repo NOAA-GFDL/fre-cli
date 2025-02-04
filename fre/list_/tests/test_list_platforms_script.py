d89224df8d3a4322723ed904744f4eb8ae20073d
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2025-02-04 17:00:01 -0500

    #315 Add validation test

--- fre/list_/tests/test_list_platforms_script.py
+++ fre/list_/tests/test_list_platforms_script.py
@@ -1,5 +1,5 @@
 """
-Test fre list exps
+Test fre list platforms
 """
 import pytest
 from pathlib import Path
@@ -9,6 +9,8 @@ from fre.list_ import list_platforms_script
 # SET-UP
 TEST_DIR = Path("fre/make/tests")
 NM_EXAMPLE = Path("null_example")
+PLATFORM = "None"
+TARGET = "None"
 YAMLFILE = "null_model.yaml"
 BADYAMLFILE = "null_model_bad.yaml"
 EXP_NAME = YAMLFILE.split(".")[0]
@@ -27,7 +29,7 @@ def test_platformyaml_exists():
     assert Path(f"{TEST_DIR}/{NM_EXAMPLE}/platforms.yaml").exists()
 
 def test_platforms_list(capfd):
-    ''' test list exps '''
+    ''' test list platforms '''
     list_platforms_script.list_platforms_subtool(f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}")
 
     #Capture output
@@ -57,24 +59,44 @@ def test_nocombinedyaml():
 # Test individual functions operating correctly: combine and clean
 def test_correct_combine():
     ''' test that combined yaml includes necesary keys '''
-    p = None
-    t = None
-    yamlfile = f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}"
+    yamlfile_path = f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}"
 
     # Combine model / experiment
-    list_platforms_script.quick_combine(yamlfile,p,t)
+    list_platforms_script.quick_combine(yamlfile_path,PLATFORM,TARGET)
     assert Path(f"{TEST_DIR}/{NM_EXAMPLE}/combined-{EXP_NAME}.yaml").exists()
 
     comb_yamlfile = f"{TEST_DIR}/{NM_EXAMPLE}/combined-{EXP_NAME}.yaml"
     with open(comb_yamlfile, 'r') as yf:
         y = yaml.load(yf,Loader=yaml.Loader)
 
-    req_keys = ["name","platform","target","fre_properties","platforms"]
+    req_keys = ["name","platform","target","platforms"]
     for k in req_keys:
         if k in y.keys():
             assert True
         else:
             assert False
+
+def test_yamlvalidate(capfd):
+    ''' test yaml is being validated '''
+    yamlfile_path = f"{TEST_DIR}/{NM_EXAMPLE}/{YAMLFILE}"
+
+    # Combine model / experiment
+    list_platforms_script.quick_combine(yamlfile_path,PLATFORM,TARGET)
+    assert Path(f"{TEST_DIR}/{NM_EXAMPLE}/combined-{EXP_NAME}.yaml").exists()
+
+    comb_yamlfile = f"{TEST_DIR}/{NM_EXAMPLE}/combined-{EXP_NAME}.yaml"
+    with open(comb_yamlfile, 'r') as yf:
+        y = yaml.load(yf,Loader=yaml.Loader)
+
+    # Validate and capture output
+    list_platforms_script.validate_yaml(y)
+    out,err=capfd.readouterr()
+    if "Intermediate combined yaml VALID." in out:
+        assert True
+    else:
+        assert False
+
+#def test_not_valid_yaml():
 
 def test_yamlremove():
    ''' test intermediate combined yaml removed '''
