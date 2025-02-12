e0a157b0040048c085dbef0033203bda1ac1f1b8
Ian Laflotte <ilaflott@gmail.com>
2025-02-12 15:06:57 -0500

    lets go with something like this for the yaml.... coalescing... tweak check script- maybe it hsould be a yamltool

--- yaml_check.py
+++ yaml_check.py
@@ -1,16 +1,33 @@
+import click
+
 import yaml
 
 from fre.yamltools.combine_yamls import join_constructor as jc
 yaml.add_constructor("!join", jc)
 
-file_obj=open("fre/tests/test_files/cmor.yaml",
-              "r+", encoding='utf-8')
 
 
-#yaml_data=yaml.safe_load(file_obj) #Loader=yaml.Loader)
-yaml_data=yaml.load(file_obj, Loader=yaml.Loader)
 
-import pprint
-pp = pprint.PrettyPrinter(indent=2)
 
-pp.pprint(yaml_data['cmor'])
+
+
+def pprint_yaml():
+    file_obj=open("fre/tests/test_files/cmor.yaml",
+                  "r+", encoding='utf-8')
+    
+    
+    #yaml_data=yaml.safe_load(file_obj) #Loader=yaml.Loader)
+    yaml_data=yaml.load(file_obj, Loader=yaml.Loader)
+    
+    import pprint
+    pp = pprint.PrettyPrinter(indent=2)
+    
+    pp.pprint(yaml_data['cmor'])
+
+
+    
+def main():
+    pprint_yaml()
+
+if __name__ == "__main__":
+    main()
