2f56f3a5b056305d59c9ef26b949509ee28ffb0e
Ian Laflotte <ian.laflotte@noaa.gov>
2025-02-21 17:14:14 -0500

    a dedicated spot for the contstructor is best i thin

---
+++ fre/yamltools/yaml_constructors.py
@@ -0,0 +1,7 @@
+def join_constructor(loader, node):
+    """
+    Allows FRE properties defined
+    in main yaml to be concatenated.
+    """
+    seq = loader.construct_sequence(node)
+    return ''.join([str(i) for i in seq])
