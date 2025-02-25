ea62768cbbca68abf2827bb3599827decc42aff6
Ian Laflotte <ilaflott@gmail.com>
2025-02-25 14:44:39 -0500

    init commit, smth small, to start draft PR

--- fre/yamltools/freyamltools.py
+++ fre/yamltools/freyamltools.py
@@ -8,39 +8,28 @@ def yamltools_cli():
     ''' entry point to fre yamltools click commands '''
 
 @yamltools_cli.command()
-@click.option("-y",
-              "--yamlfile",
-              type=str,
-              help="YAML file to be used for parsing",
-              required=True)
-@click.option("-e",
-              "--experiment",
-              type=str,
+@click.option("-y", "--yamlfile", type=str,
+              help="YAML file to be used for parsing", required=True)
+@click.option("-e", "--experiment", type=str,
               help="Experiment name")
-@click.option("-p",
-              "--platform",
-              type=str,
-              help="Platform name",
-              required=True)
-@click.option("-t",
-              "--target",
-              type=str,
-              help="Target name",
-              required=True)
-@click.option("--use",
-              type=click.Choice(['compile','pp']),
-              help="Process user is combining yamls for. Can pass 'compile' or 'pp'",
-              required=True)
-@click.option("-o",
-              "--output",
-              type=str,
+@click.option("-p", "--platform", type=str,
+              help="Platform name", required=True)
+@click.option("-t", "--target", type=str,
+              help="Target name", required=True)
+@click.option("--use", type=click.Choice(['compile','pp', 'cmor']),
+              help="Process user is combining yamls for. Can pass 'compile', 'pp', or 'cmor'", required=True)
+@click.option("-o", "--output", type=str,
               help="Output")
-def combine_yamls(yamlfile,experiment,platform,target,use,output):
-    """ 
+def combine_yamls(yamlfile,
+                  experiment, platform, target,
+                  use, output):
+    """
     - Combine the model yaml with the compile, platform,
     experiment, and analysis yamls
     """
-    combine_yamls_script.consolidate_yamls(yamlfile,experiment,platform,target,use,output)
+    combine_yamls_script.consolidate_yamls(yamlfile,
+                                           experiment, platform, target,
+                                           use, output)
 
 if __name__ == "__main__":
     yamltools_cli()
