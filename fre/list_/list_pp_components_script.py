14a71d5d57129442de07de103d0b5e1749d7641f
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2025-03-26 12:45:32 -0400

    #386 Get rid of output combined yaml file (dont need to use it)

--- fre/list_/list_pp_components_script.py
+++ fre/list_/list_pp_components_script.py
@@ -2,18 +2,14 @@
 Script parses combined yaml dictionary to list components that will be post-processed
 """
 
-import logging
-fre_logger = logging.getLogger(__name__)
-
-from pathlib import Path
-
 # this brings in the yaml module with the join_constructor
 # this is defined in the __init__
 from fre.yamltools import *
 
-
-from fre.yamltools.helpers import yaml_load
 from fre.yamltools import combine_yamls_script as cy
+
+import logging
+fre_logger = logging.getLogger(__name__)
 
 def quick_combine(yml, exp, platform, target, use, output):
     """
@@ -27,16 +23,7 @@ def quick_combine(yml, exp, platform, target, use, output):
                                          target = target,
                                          use = use,
                                          output = output)
-
-def remove(combined):
-    """
-    Remove intermediate combined yaml.
-    """
-    if Path(combined).exists():
-        Path(combined).unlink()
-        fre_logger.info(f"Intermediate combined yaml {combined} removed.")
-    else:
-        raise ValueError(f"{combined} could not be found to remove.")
+    return full_yamldict
 
 def list_ppcomps_subtool(yamlfile, experiment):
     """
@@ -47,13 +34,8 @@ def list_ppcomps_subtool(yamlfile, experiment):
     p = "None"
     t = "None"
 
-    combined = f"combined-{e}.yaml"
-
     # Combine model / experiment
-    yml_file = quick_combine(yamlfile,e,p,t,"pp",output=combined)
-
-    # load the yaml we made
-    yf = yaml_load(combined)
+    yml_dict = quick_combine(yamlfile,e,p,t,"pp",output=None)
 
     # set logger level to INFO
     former_log_level = fre_logger.level
@@ -61,13 +43,10 @@ def list_ppcomps_subtool(yamlfile, experiment):
 
     # log the experiment names, which should show up on screen for sure
     fre_logger.info("Components to be post-processed:")
-    for i in yf["postprocess"]["components"]:
-        if i.get("do_postprocess") == True:
+    for i in yml_dict["postprocess"]["components"]:
+        if i.get("do_postprocess") is True:
             fre_logger.info(f'   - {i.get("type")}')
     fre_logger.info("\n")
 
     # set logger back to normal level
     fre_logger.setLevel(former_log_level)
-
-    # remove intermediate combined yaml
-    remove(combined)
