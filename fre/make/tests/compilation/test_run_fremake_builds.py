c173c44a8c1eab26803ff94774925e9e605bdb26
Ryan Mulhall <Ryan.Mulhall@gaea53.ncrc.gov>
2025-03-28 17:04:32 -0400

    use explicit argument name for verbose too

--- fre/make/tests/compilation/test_run_fremake_builds.py
+++ fre/make/tests/compilation/test_run_fremake_builds.py
@@ -57,7 +57,7 @@ def test_run_fremake_serial_compile():
     os.environ["TEST_BUILD_DIR"] = SERIAL_TEST_PATH
     run_fremake_script.fremake_run(YAMLPATH, PLATFORM, TARGET,
         parallel=False, jobs=1, no_parallel_checkout=False,
-        no_format_transfer=True, execute=True, VERBOSE)
+        no_format_transfer=True, execute=True, verbose=VERBOSE)
     assert Path(f"{SERIAL_TEST_PATH}/fremake_canopy/test/{EXPERIMENT}/{PLATFORM[0]}-{TARGET[0]}/exec/{EXPERIMENT}.x").exists()
 
 # same test with a parallel build
@@ -67,7 +67,7 @@ def test_run_fremake_multijob_compile():
     os.environ["TEST_BUILD_DIR"] = MULTIJOB_TEST_PATH
     run_fremake_script.fremake_run(YAMLPATH, PLATFORM, TARGET,
         parallel=True, jobs=4, no_parallel_checkout=False,
-        no_format_transfer=False, execute=True, VERBOSE)
+        no_format_transfer=False, execute=True, verbose=VERBOSE)
     assert Path(f"{MULTIJOB_TEST_PATH}/fremake_canopy/test/{EXPERIMENT}/{PLATFORM[0]}-{TARGET[0]}/exec/{EXPERIMENT}.x").exists()
 
 # containerized build
@@ -76,7 +76,7 @@ def test_run_fremake_container_build():
     ''' checks image creation for the container build'''
     run_fremake_script.fremake_run(YAMLPATH, CONTAINER_PLATFORM, TARGET,
         parallel=False, jobs=1, no_parallel_checkout=True,
-        no_format_transfer=false, execute=True, VERBOSE)
+        no_format_transfer=false, execute=True, verbose=VERBOSE)
     assert Path("null_model_full-debug.sif").exists()
 
 @pytest.mark.skipif(not has_podman, reason="missing podman")
@@ -84,4 +84,4 @@ def test_run_fremake_container_build_notransfer():
     ''' checks image creation with the .sif transfer turned off '''
     run_fremake_script.fremake_run(YAMLPATH, CONTAINER_PLATFORM, TARGET,
         parallel=False, jobs=1, no_parallel_checkout=True,
-        no_format_transfer=false, execute=True, VERBOSE)
+        no_format_transfer=false, execute=True, verbose=VERBOSE)
