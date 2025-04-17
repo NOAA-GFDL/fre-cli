bb14afdd84272a757b3141f512ef911abbe7c74b
Dana Singh <Dana.Singh@an103.princeton.rdhpcs.noaa.gov>
2025-04-17 10:26:34 -0400

    #221 Update `fremake_parallel` to `run`

--- fre/make/gfdlfremake/buildBaremetal.py
+++ fre/make/gfdlfremake/buildBaremetal.py
@@ -8,13 +8,41 @@
 import subprocess
 import os
 
-def fremake_parallel(fremakeBuildList):
+def run(cs):
     """
-    Brief: Called for parallel execution purposes.  Runs the builds.
+    Brief: Run the build script
     Param:
-        - fremakeBuildList : fremakeBuild object list passes by pool.map
+        - cs : The created compile script
     """
-    fremakeBuildList.run()
+    #command = [bld_dir, "compile.sh"]
+    command = cs
+    bld_dir = os.path.dirname(command)
+
+    # Run compile script
+    p1 = subprocess.Popen(command, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+
+    # Direct output to log file as well
+    p2 = subprocess.Popen(["tee",bld_dir+"/log.compile"], stdin=p1.stdout)
+
+    # Allow process1 to receive SIGPIPE is process2 exits
+    p1.stdout.close()
+    p2.communicate()
+
+    # wait for process1 to finish before checking return code
+    p1.wait()
+    if p1.returncode != 0:
+        print(f"\nThere was an error running {bld_dir}/compile.sh")
+        print(f"Check the log file: {bld_dir}/log.compile")
+    else:
+        print(f"\nSuccessful run of {bld_dir}/compile.sh")
+
+#def fremake_parallel(fremakeBuildList):
+#    """
+#    Brief: Called for parallel execution purposes.  Runs the builds.
+#    Param:
+#        - fremakeBuildList : fremakeBuild object list passes by pool.map
+#    """
+#    fremakeBuildList.run()
 
 class buildBaremetal():
     """
