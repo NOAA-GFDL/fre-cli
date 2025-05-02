049a90b30c0c6fd0a9ab804087a8459d7d044cc1
Ciheim Brown <Ciheim.Brown@an106.princeton.rdhpcs.noaa.gov>
2025-05-02 14:39:00 -0400

    Fixing some things

--- fre/pp/ppval_script.py
+++ fre/pp/ppval_script.py
@@ -1,4 +1,4 @@
-''' This script will determine an estimated number of timesteps from a postprocessed time-series file's name and run nccheck on it'''
+''' This script will determine an estimated number of timesteps from a postprocessed time-series file's name and run nccheck on it '''
 
 import os
 import logging
@@ -14,6 +14,10 @@ fre_logger = logging.getLogger(__name__)
 # Get estimated number of timesteps
 def getenot(date_start,date_end,chunk_type,cal):
 
+    #Chunk type is the frequency of the data chunk
+    #enot = estimated number of timesteps
+    #start/stop are cf datetime objects representing the start and end time of the data chunk
+    #diff represents the time difference between start and stop
     if chunk_type == 'yearly':
         enot = int(date_end[1]) - int(date_start[1])+ 1
 
@@ -52,13 +56,16 @@ def getenot(date_start,date_end,chunk_type,cal):
     
     return enot
 
+# Filepath is the path to the time-series file to be checked
 def validate(filepath):
-    """ Compares the number of timesteps in each netCDF (.nc) file to the number of expected timesteps as found the file name """
+    """ Compares the number of timesteps in each netCDF (.nc) file to the number of expected timesteps as found the filename. """
+
 
     import re
     # Get the date range from the filename
+    # This regular expression accepts at mininum '.YYYY-YYYY.' date strings.
+    # If month, day, hour, and minute strings are present it will identify them by looking for groups of two digits after the year string
     match = re.compile("\.((?:\d{4})(?:\d{2}(?:\d{2}(?:\d{2}(?::\d{2})?)?)?)?)-((?:\d{4})(?:\d{2}(?:\d{2}(?:\d{2}(?::\d{2})?)?)?)?)\.")
-    #match = re.compile("\.(\d{4}(?:\d{2}))-(\d{4}(?:\d{2}))\.")
     filename = os.path.basename(filepath)
     date_range = match.search(filename)
 
@@ -66,10 +73,23 @@ def validate(filepath):
     # date_range[0] is the full match (e.g., ".202201-202501."
     # date_range[1] is the start date (e.g., "202201")
     # date_range[2] is the end date (e.g., "202501")
+    # This regular expression captures date start/end individually by first capturing the year as a 4 digit number then capturing each following group of two digits
+    # Minute string is identified by ':' followed with two digits 
     d_regex = re.compile(r"(\d{4})(\d{2})?(\d{2})?(\d{2})?(?::(\d{2}))?")
-    #d_regex = re.compile("(\d{4})(\d{2})?(\d{2})?(\d{2})?(?::\d{2})?")
     date_end = d_regex.search(date_range[2])
     date_start = d_regex.search(date_range[1])
+
+    # Get calendar type from metadata and make sure it's valid
+    # dataset is a netCDF4 Dataset object created from the given file
+    dataset = netCDF4.Dataset(filepath, 'r')
+    # Cal is the calendar type read from the file's metadata
+    cal = dataset.variables['time'].calendar.lower()
+
+    # Check if the calendar name is valid by creating a test datetime object.. if it's not raise an error
+    try:
+        cftime.datetime(1,1,1, calendar = cal)
+    except:
+        raise ValueError(f" Calendar name must follow cf convention for validation. {cal} is not a valid calendar.")
 
     # date_{end,start} will have a total of date_end.lastindex groups, 
     # that are the year (date_end[1]), the month (date_end[2]), the
@@ -77,13 +97,7 @@ def validate(filepath):
     # You can use the value of date_end.lastindex to know if this is
     # a yearly, monthly, daily, or sub-daily file.
 
-    # Get calendar type from metadata and make sure it's valid
-    dataset = netCDF4.Dataset(filepath, 'r')
-    cal = dataset.variables['time'].calendar.lower()
-    try:
-        cftime.datetime(1,1,1, calendar = cal)
-    except:
-            raise ValueError("Calendar name must follow cf convention for validation")
+    # Result is the return code from the nccheck script (0 = Expected number of timesteps found, 1 = Unexpected number of timesteps found) 
 
     # YEARLY
     if date_end.lastindex == 1:
@@ -101,6 +115,7 @@ def validate(filepath):
         result = ncc.check(filepath,enot)
 
     # We would rather not check filepaths but it's necessary for sub-daily files
+    # Path elements contains the directories from the filepath.. we use this to determine frequency/chunk_size in sub-daily files
     path_elements = os.path.abspath(filepath).split('/')
 
     # 4x Daily
@@ -124,7 +139,7 @@ def validate(filepath):
         result = ncc.check(filepath,enot)
 
     if result == 1:
-                fre_logger.error(f" Timesteps found in {filepath} differ from expectation")
+        fre_logger.error(f" Timesteps found in {filepath} differ from expectation")
     return result
 
 if __name__ == '__main__':
