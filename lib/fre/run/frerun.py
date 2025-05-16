20cccb80868221683acca91a3c62013bfd6d6e28
ilaflott <ilaflott@users.noreply.github.com>
2025-05-16 14:04:23 +0000

    deploy: 9bfe55b82b7d1b170f538b188aa1dbbf4f2ab784

---
+++ lib/fre/run/frerun.py
@@ -0,0 +1,20 @@
+'''
+entry point for fre run subcommands
+'''
+
+import click
+from .frerunexample import run_test_function
+
+@click.group(help=click.style(" - run subcommands !!!NotImplemented!!!", fg=(164,29,132)))
+def run_cli():
+    ''' entry point to fre run click commands '''
+
+@run_cli.command()
+@click.option('--uppercase', '-u', is_flag=True, help = 'Print statement in uppercase.')
+def function(uppercase):
+    """ - Execute fre run test """
+    run_test_function(uppercase)
+    raise NotImplementedError('fre run has not been implemented yet!')
+
+if __name__ == "__main__":
+    run_cli()
---
+++ lib/fre/run/frerunexample.py
@@ -0,0 +1,15 @@
+"""
+experimentation file for integrating one file's functions into main prototype fre file
+authored by Bennett.Chang@noaa.gov | bcc2761
+NOAA | GFDL
+"""
+
+def run_test_function(uppercase=None):
+    """Execute fre run run_test_function"""
+    statement = "testingtestingtestingtesting"
+    if uppercase:
+        statement = statement.upper()
+    print(statement)
+
+if __name__ == '__main__':
+    run_test_function()
