d130cebadd92ab7a6ee663bf631652290aaeba06
kiihne-noaa <Avery.Kiihne@saic.com>
2025-07-11 11:23:05 -0400

    Update cdoTimeAverager.py

--- fre/app/generate_time_averages/cdoTimeAverager.py
+++ fre/app/generate_time_averages/cdoTimeAverager.py
@@ -13,9 +13,9 @@ class cdoTimeAverager(timeAverager):
         use cdo package routines via python bindings        
 
         :param self: This is an instance of the class cdoTimeAverager
-        :param infile: path to history file, or list of paths
+        :param infile: path to history file, or list of paths, default is None
         :type infile: str, list
-        :param outfile: path to where output file should be stored
+        :param outfile: path to where output file should be stored, default is None
         :type outfile: str
         :return: 1 if the instance variable self.avg_typ is unsupported, 0 if function has a clean exit
         :rtype: int
