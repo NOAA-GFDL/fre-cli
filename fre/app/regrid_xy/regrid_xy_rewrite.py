ed59233bdae1a338ce48aa266f8e45591f16bc95
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-07-29 15:56:41 -0400

    save

--- fre/app/regrid_xy/regrid_xy_rewrite.py
+++ fre/app/regrid_xy/regrid_xy_rewrite.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+import subprocess 
 import tarfile
 import xarray as xr
 import yaml
 
 #dictionary that maps inputRealm from the pp_yaml the variable name
-#holding the mosaic filenames in the gridspec file
-#For example, in gridspec.nc, atm_mosaic_file = C96_mosaic.nc
+#holding the mosaic filenames in the grid_spec file
+#For example, in grid_spec.nc, atm_mosaic_file = C96_mosaic.nc
 #For land, lnd_mosaic_file = C96_mosaic.nc
 #For ocn, ocn_mosaic_file = "ocean_mosaic.nc"
 map_mosaicfile = dict(atmos = 'atm_mosaic_file',
@@ -21,70 +22,70 @@ non_regriddable_variables = [
     'areacello_cu', 'areacello_cv', 'areacello_bu', 'average_T1','average_T2',
     'average_DT','time_bnds']
 
-def get_input_dimensions(input_file):
-
-    """
-    Retrieves the grid dimension 'nx' and 'ny' from the input_file
-    Returns the number of grid cells on the A-grid
-    """
-
-      grid = xr.load_dataset(input_file)
-      nlon = grid.sizes['nx']//2
-      nlat = grid.sizes['ny']//2
-
-      return [nlat, nlon]
-  
-
-def get_input_file(input_file: str = None, input_date: str = ''):
+def get_input_file(input_file: str = None, 
+                   input_date: str = ''):
 
     """
     Intakes the prefix of the input_file returns the filename
     pretended with the input_date so that the filename is YYYYMMDD.input_file.  
-    For a cubed sphere, fregrid will regrid the tile files YYYYMMDD.input_file.tile#.nc
+    For a cubed sphere, fregrid will append the tile number to the filename 
+    such that fregrid will regrid the variables in YYYYMMDD.input_file.tile#.nc
     """
     
     #FRE-NCTools expects input files without the .nc extension
     if ".nc" in input_file: input_file = input_file[:-3]
 
-    return f"{inputdate}.{inputfile}"
+    return f"{input_date}.{input_file}"
 
 
-def get_input_mosaic(gridspec: str, input_realm: str):
+def get_input_mosaic(grid_spec: str, 
+                     input_realm: str):
 
     """
     Intakes the input_realm field from the pp yaml and returns
-    the type of mosaic file
+    the variable in the grid_spec file that contains the mosaic file information
     """
     
     mosaic_key = map_mosaicfile[input_realm]
     return str(xr.load_dataset(grid_spec)[mosaic_key].values.astype(str))
+  
+
+def is_tiled(mosaic_file: str):
+  
+  if xr.load_dataset(str(mosaic_file)).sizes["ntiles"] > 1: return True
+  return False
 
 
-def get_remap_file(remap_file: str, output_dir: str, input_mosaic, output_nlat: int, output_nlon: int):
+def get_remap_file(remap_file: str, 
+                   input_mosaic: str, 
+                   output_nlat: int, 
+                   output_nlon: int,
+                   interp_method: str):
 
+    remap_cache_file = f"{input_mosaic}Xnlon{output_nlon}nlat{output_nlat}_{interp_method}.nc"
     if remap_file is not None:
         try:
-            shutil.copy(remap_file, remap_file.split('/').pop())
+          shutil.copy(remap_file, remap_cache_file)
+          fre_logger.info(f"Copied {remap_file} to {remap_cache_file}")
         except Exception as exc:
-            raise OSError("remap_file={remap_file} could not be copied to work dir") from exc
+            fre_logger.info(f"Cannot find specified remap_file {remap_file}\n"
+                            "Remap file {remap_cache_file} will be generated on-the-fly")
 
-        remap_file= f"fregrid_remap_file_{output_nlon}_by_{output_nlat}.nc"
-        remap_cache_file = f"{output_dir}/{input_moasic}X{nlon}_{nlat}_conservative{order}.nc"
-
-        fre_logger.info(f'remap_file = {remap_file}\n')
-        fre_logger.info(f'NOTE: Will generate remap file and cache to {remap_cache_file}')
-        
-        return remap_file, remape_cache_file
+    return remap_cache_file
 
 
-def get_scalar_fields(input_file: str):
+def get_scalar_fields(input_file: str, is_tiled: bool = True):
 
     """
     Returns all the regriddable variables in an input file
     """
     
+    if is_tiled: input_file += ".tile1"
+    input_file += ".nc"
+    
     #load data file
-    dataset = xr.load_dataset(input_file).drop_vars(non_regriddable_variables, errors="ignore")
+    #xarray gives an error if variables to drop do not exist in the file.  The errors="ignore" fixes that
+    dataset = xr.load_dataset(str(input_file)).drop_vars(non_regriddable_variables, errors="ignore")
 
     #list of variables to regrid, only multi-dimensional data will be regridded
     regrid_vars_string = " ".join([variable for variable in dataset if len(dataset[variable].sizes)>1])
@@ -92,85 +93,96 @@ def get_scalar_fields(input_file: str):
     return regrid_vars_string
     
 
-def regrid_xy(pp_yaml_file: dict,
+def regrid_xy(pp_yamlfile: str,
               remap_file: str = None,
-              regrid_thesecomponents: list[int] = None,
+              only_thesecomponents: list[int] = None,
               input_date: str = None,
               input_dir: str = None,
               output_dir: str = None):
     
+    """
+    Remaps variables for each component element in the post-processing yaml file.
+    If regrid_thesecomponents is not specified, regrid_xy will regrid all components.
+    To regrid only specific components, regrid_thesecomponents must have the element number
+    of the component to regrid.      
+    """
+    
     #load pp_yaml_file to a dictionary
-    with open(pp_yaml_file, 'r') as openedfile:
+    with open(pp_yamlfile, 'r') as openedfile:
         ppyaml = yaml.safe_load(openedfile)
 
-    #untar gridspec tar file
-    pp_gridspec_tar = ppyaml['postprocess']['settings']['pp_grid_spec']
-
-    if tarfile.is_tarfile(pp_gridspec_tar):
-        with tarfile.open(pp_gridspec_tar, 'r') as tar: tar.extractall()         
-
-    #set input and output directories
-    input_dir = ppyaml['directories']["history_dir"]
-    output_dir = ppyaml['directories']["pp_dir"]
-
-    #set remap file #f"{output_dir}/remap.nc"
-    remap_file = get_remap_file(remap_file, nlon, nlat)
+    #retrieve the grid_spec tar filename from ppyaml
+    pp_grid_spec_tar = ppyaml['postprocess']['settings']['pp_grid_spec']
     
+    #untar grid_spec tar file
+    if tarfile.is_tarfile(pp_grid_spec_tar):
+        with tarfile.open(pp_grid_spec_tar, 'r') as tar: tar.extractall()         
 
-    #get components to remap
-    if regrid_thesecomponents is None:
+    #TODO check if grid_spec.nc exists
+    grid_spec = "grid_spec.nc"
+
+    #set input and output directory names if not specified 
+    if input_dir is None: input_dir = ppyaml['directories']["history_dir"]
+    if output_dir is None: output_dir = ppyaml['directories']["pp_dir"]    
+
+    #get components to remap 
+    if only_thesecomponents is None:
+        #regrid all components
         components = ppyaml['postprocess']['components']
     else:
+        #regrid only components specified in the list only_thesecomponents
         components = [ppyaml['postprocess']['components'][i] for i in regrid_thesecomponents]
 
-    #iterate over components to regrid
+    #iterate over components that will be regridded
     for component in components:
 
-        #skip is postprocessing is turned off for the component
+        #skip if postprocessing is turned off for the component
         if component['postprocess_on'] is False: continue
 
-        #get input mosaic file corresponding to the inputRealm
-        input_mosaic = get_input_mosaic(gridspec, component['inputRealm'])
-
+        #get input mosaic filename corresponding to the inputRealm
+        input_mosaic = get_input_mosaic(grid_spec, component['inputRealm'])
+        tiled = is_tiled(input_mosaic)
+        
         #get interp_method: bilinear, conserve_interp1, conserve_interp2
         interp_method = component['interpMethod']
 
         #get output grid dimensions
-        [output_nlat, output_nlon] = component['xyInterp']
+        [output_nlat, output_nlon] = [int(dims) for dims in component['xyInterp'].split(",")]
         
-        #static file with input grid areas
+        #directory holding the static file that contains the input grid cell areas
         associated_file_dir = input_dir
 
-        #get history file prefixes to regrid
-        for ifile in component['sources']['history_file']:
+        #remap each history file in component.  The history file will be
+        #specified as, e.g., "atmos_daily_cmip"
+        for history_dict in component['sources']:
 
-            #get names of history files (labeled by input_date)
-            input_file = get_input_file(input_file, input_date)
+          history_file = history_dict["history_file"]
 
-            #get input grid dimensions
-            [input_nlat, input_nlon] = get_input_dimensions(input_file)
-
-            #get remap file and name that will be cached
-            remap_file, remap_cache_file = get_remap_file(remap_file, input_mosaic, output_nlat, output_nlon)
+          #pretend the date to the history_file so that, e.g.,
+          input_file = get_input_file(history_file, input_date)
+          
+          #get all fields to regrid
+          scalar_field = get_scalar_fields(input_file, tiled)        
+          
+          #get remap file and name that will be cached
+          remap_cache_file = get_remap_file(remap_file, input_mosaic, output_nlat, output_nlon, interp_method)
             
-            #get all fields to regrid
-            scalar_field = get_scalar_fields(input_file)        
-
-            fregrid_command = ['fregrid',
-                               '--debug',
-                               '--standard_dimension',
-                               '--input_mosaic', input_mosaic
-                               '--input_dir', input_dir,
-                               '--input_file', input_file,
-                               '--associated_file_dir', associated_file_dir,
-                               '--interp_method', interp_method,
-                               '--remap_file', remap_file,
-                               '--nlon', output_nlon,
-                               '--nlat', output_nlat,
-                               '--scalar_field', scalar_field,
-                               '-output_file', output_file
-            ]
+          output_file = "this"
+            
+          fregrid_command = ['fregrid',
+                              '--debug',
+                              '--standard_dimension',
+                              '--input_mosaic', input_mosaic,
+                              '--input_dir', input_dir,
+                              '--input_file', input_file,
+                              '--associated_file_dir', associated_file_dir,
+                              '--interp_method', interp_method,
+                              '--remap_file', remap_cache_file,
+                              '--nlon', output_nlon,
+                              '--nlat', output_nlat,
+                              '--scalar_field', scalar_field,
+                              '--output_dir', output_dir,
+                              '-output_file', output_file]
+          print(fregrid_command)
                                
-            fregrid_proc = subprocess.run(fregrid_command, check=True)
-            
-regrid_xy('./c96L65_am5f8d6r3_amip.yaml')
+            #fregrid_proc = subprocess.run(fregrid_command, check=True)
