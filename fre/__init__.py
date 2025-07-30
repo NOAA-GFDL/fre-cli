92adc4b914fbce6d4c2ccfd345de7e1c7b5da2ea
Seth Underwood <Seth.Underwood@noaa.gov>
2025-07-30 13:19:17 -0400

    An idea on how to get the version number
    This is ta allow the version to be dynamic, and easier to define in a single location.

--- fre/__init__.py
+++ fre/__init__.py
@@ -1,22 +1,9 @@
-import importlib.metadata
+import os
 
-# versioning, turn xxxx.y into xxxx.0y
-version_unexpanded = importlib.metadata.version('fre-cli')
-version_unexpanded_split = version_unexpanded.split('.')
-if len(version_unexpanded_split[1]) == 1:
-    version_minor = "0" + version_unexpanded_split[1]
-else:
-    version_minor = version_unexpanded_split[1]
-# if the patch version is present, then use it. otherwise, omit
 try:
-    len(version_unexpanded_split[2])
-    if len(version_unexpanded_split[2]) == 1:
-        version_patch = "0" + version_unexpanded_split[2]
-    else:
-        version_patch = version_unexpanded_split[2]
-    version = version_unexpanded_split[0] + '.' + version_minor + '.' + version_patch
-except IndexError:
-    version = version_unexpanded_split[0] + '.' + version_minor
+    version = os.getenv("GIT_DESCRIBE_TAG", "2025.04")
+except:
+    version = "unknown"
 
 __version__=version
 
