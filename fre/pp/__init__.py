f48f7dae3d92f8a1215196273e0d7f8145858ecf
Ian Laflotte <ilaflott@gmail.com>
2025-08-08 13:16:23 -0400

    docstring for fre.pp.make_workflow_name

--- fre/pp/__init__.py
+++ fre/pp/__init__.py
@@ -1,17 +1,32 @@
-def make_workflow_name(experiment= None,
-                       platform  = None,
-                       target    = None):
-    '''
+from typing import Optional
+
+def make_workflow_name(experiment : Optional[str] = None,
+                       platform : Optional[str] = None,
+                       target : Optional[str] = None) -> Optional[str]:
+    """
     function that takes in a triplet of tags for a model experiment, platform, and target, and
     returns a directory name for the corresponding pp workflow. because this is often given by 
     user to the shell being used by python, we split/reform the string to remove semi-colons or
     spaces that may be used to execute an arbitrary command with elevated priveleges.
-    inputs: 
-        experiment, platform, target are all required inputs and strings that will be parsed for 
-        hostile directions
-    outputs:
-        the desired pp workflow directory name as a string
-    '''
+
+    Parameters
+    ----------
+    experiment : str
+        string representing the name of an experiment
+    platform : str
+        string representing the name of a platform
+    target : str
+        string representing the name of a compilation target
+
+    Returns
+    -------
+    string
+        string created in specific format from the input strings
+
+    Notes
+    -----
+    - does not check for inputs for None value, and will use it to create a return object
+    """
     name = f'{experiment}__{platform}__{target}'
     return ''.join(
                       (''.join(
