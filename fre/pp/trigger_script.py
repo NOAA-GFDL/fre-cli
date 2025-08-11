11083243070553b5d48e40cd3a5af427c6412c1f
Carolyn Whitlock <carolyn.whitlock@noaa.gov>
2025-08-11 16:23:43 -0400

    typo in nites

--- fre/pp/trigger_script.py
+++ fre/pp/trigger_script.py
@@ -16,7 +16,7 @@ def trigger(experiment = None, platform = None, target = None, time = None):
     :type target: str
     :param time: The start time of the segment. Formatted as a series of integers.
     :type time: Cylc representation of a time point
-    ..note: 
+    .. note: 
     The segment is defined as a start point (--time) and a duration (defined in
     the experiment yaml). Cylc combines the two for a cycle duration; we are using
     datetime cycling (https://cylc.github.io/cylc-doc/stable/html/glossary.html#term-datetime-cycling)
