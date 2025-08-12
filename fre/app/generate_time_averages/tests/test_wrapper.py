3b2979884c53c2c5d9b8545ddab11135cab3516b
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2025-08-12 16:03:48 -0400

    Completed test for monthly history to annual climo

--- fre/app/generate_time_averages/tests/test_wrapper.py
+++ fre/app/generate_time_averages/tests/test_wrapper.py
@@ -2,8 +2,14 @@ import pytest
 import subprocess
 from pathlib import Path
 import xarray as xr
+import metomi.isodatetime.parsers
 
 from .. import wrapper
+
+cycle_point = '1980-01-01'
+output_interval = 'P2Y'
+input_interval  = 'P1Y'
+grid = '180_288.conserve_order2'
 
 @pytest.fixture()
 def create_input_files(tmp_path):
@@ -26,9 +32,13 @@ def create_input_files(tmp_path):
     for file_ in input_files:
         assert file_.exists()
 
+    # write shard directory structure
+    output_dir = Path(tmp_path, 'ts', grid, 'atmos_month', 'P1M', input_interval)
+    output_dir.mkdir(parents=True)
+
     # write netcdfs from the cdfs
     for file_ in input_files:
-        output_file = tmp_path / file_.stem
+        output_file = output_dir / file_.stem
         output_file = Path(str(output_file) + '.nc')
         command = ['ncgen', '-o', output_file, file_]
         sp = subprocess.run(command, check=True)
@@ -41,4 +51,16 @@ def test_wrapper(create_input_files):
     """
     Run climatology wrapper and verify output.
     """
-    assert False
+    sources = ['atmos_month']
+    frequency = 'yr'
+
+    wrapper.generate_wrapper(cycle_point, create_input_files, sources, output_interval, input_interval, grid, frequency)
+
+    output_dir = Path(create_input_files, 'av', grid, 'atmos_month', 'P1Y', output_interval)
+    output_files = [
+        output_dir / 'atmos_month.1980-1981.alb_sfc.nc',
+        output_dir / 'atmos_month.1980-1981.aliq.nc'
+    ]
+
+    for file_ in output_files:
+        assert file_.exists()
