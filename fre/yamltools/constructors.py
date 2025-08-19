7961f72b3c0ec73af03c411bd3a809ec1bdcadea
Ian L. <6273252+ilaflott@users.noreply.github.com>
2025-08-19 14:22:59 -0400

    Update constructors.py

--- fre/yamltools/constructors.py
+++ fre/yamltools/constructors.py
@@ -6,9 +6,9 @@ import yaml
 def _join_constructor( loader: yaml.Loader,
                       node: yaml.Node ) -> str:
     """
-    Allows strings defined as elements in a list, specified in a yaml, to be concatenated together. this constructor
-    is generally getting associated with the default yaml.Loader used by the pyyaml module. This function is not generally
-    called directly. see fre.yamltools' __init__.py for more information.
+    Allows strings defined as elements in a list, specified in a yaml, to be concatenated together. This function is not
+    intended for direct function-calling with arguments, but rather, intended to be added to the loaded yaml module as a 
+    constructor. See https://pyyaml.org/wiki/PyYAMLDocumentation and fre.yamltools' __init__.py for more information.
 
     :param loader: a yaml.Loader object
     :type loader: yaml.Loader
@@ -17,7 +17,7 @@ def _join_constructor( loader: yaml.Loader,
     :return: Concatenated string from the list elements
     :rtype: str
 
-    .. note:: see https://pyyaml.org/wiki/PyYAMLDocumentation for more info on yaml constructors
+    .. note:: see https://pyyaml.org/wiki/PyYAMLDocumentation for more info on yaml constructors and Loaders
     .. note:: see other files in fre.yamltools that import this constructor and the yaml module at the same time
     .. note:: the constructor gets called in a yamlfile like !join[ ... ]
     .. note:: this will result in an attempt to resolve aliases and other complex objects to their final ascii values
