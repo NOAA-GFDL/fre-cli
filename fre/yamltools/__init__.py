0f1c439768d374e3379a0c51bd751205b3a66f88
Ian L. <6273252+ilaflott@users.noreply.github.com>
2025-08-19 14:20:07 -0400

    Update __init__.py

--- fre/yamltools/__init__.py
+++ fre/yamltools/__init__.py
@@ -1,6 +1,8 @@
-
-# other scripts in here use this with a 'from . import *'
-# this brings in the yaml module with the join_constructor
+"""
+other bits in fre.yamltools use this with a 'from . import *'. This way, the 
+yaml module understands what to do with the '!join' references in our yaml files
+when they appear.
+"""
 import yaml
-from .constructors import join_constructor
-yaml.add_constructor('!join', join_constructor)
+from .constructors import _join_constructor
+yaml.add_constructor('!join', _join_constructor)
