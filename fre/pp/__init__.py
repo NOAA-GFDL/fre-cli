a605ea0d3ed2a50dbe47e8791c1deba867eeebc7
Ian L. <6273252+ilaflott@users.noreply.github.com>
2025-08-19 11:24:23 -0400

    Update __init__.py

--- fre/pp/__init__.py
+++ fre/pp/__init__.py
@@ -2,10 +2,10 @@ from typing import Optional
 
 def make_workflow_name(experiment : Optional[str] = None,
                        platform : Optional[str] = None,
-                       target : Optional[str] = None) -> Optional[str]:
+                       target : Optional[str] = None) -> str:
     """
-    function that takes in a triplet of tags for a model experiment, platform, and target, and
-    returns a directory name for the corresponding pp workflow. because this is often given by 
+    Function that takes in a triplet of tags for a model experiment, platform, and target, and
+    returns a directory name for the corresponding pp workflow. Because this is often given by 
     user to the shell being used by python, we split/reform the string to remove semi-colons or
     spaces that may be used to execute an arbitrary command with elevated privileges.
 
@@ -18,7 +18,7 @@ def make_workflow_name(experiment : Optional[str] = None,
     :return: string created in specific format from the input strings
     :rtype: str
 
-    .. note:: does not check for inputs for None value, and will use it to create a return object
+    .. note:: if any arguments are None, then "None" will appear in the workflow name
     """
     name = f'{experiment}__{platform}__{target}'
     return ''.join(
