6fce01e872ccd2a9bda67b0f716403bb1f2a2020
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2025-08-26 10:55:49 -0400

    Add test for monthly average combining

--- fre/app/generate_time_averages/tests/test_combine.py
+++ fre/app/generate_time_averages/tests/test_combine.py
@@ -38,15 +38,61 @@ def create_annual_per_variable_climatologies(tmp_path):
 
     yield tmp_path
 
+@pytest.fixture()
+def create_monthly_per_variable_climatologies(tmp_path):
+    """
+    Create per-variable climatologies.
+
+    in/atmos/P1M/P2Y/alb_sfc.[01-12].nc
+    in/atmos/P1M/P2Y/aliq.[01-12].nc
+    """
+    # path to input CDL files
+    input_dir = Path('fre/tests/test_files/climatology/outputs/monthly')
+
+    # two variables
+    input_basenames = [
+        'atmos.1980-1981.alb_sfc',
+        'atmos.1980-1981.aliq'
+    ]
+
+    # path to test output NC files
+    output_dir = Path(tmp_path, 'in', 'atmos', 'P1M', 'P2Y')
+    output_dir.mkdir(parents=True)
+
+    # write netcdf files
+    for i in range(1,13):
+        for name in input_basenames:
+            input_file = input_dir / f"{name}.{i:02d}.cdl"
+            assert input_file.exists()
+
+            output_file = output_dir / f"{name}.{i:02d}.nc"
+            command = ['ncgen', '-o', output_file, input_file]
+            sp = subprocess.run(command, check=True)
+            assert sp.returncode == 0
+            assert output_file.exists()
+
+    yield tmp_path
+
 def test_combine_annual_av(create_annual_per_variable_climatologies):
     """
     Combine per-variable annual climatologies into combined annual climatology file
     """
 
-    Path(create_annual_per_variable_climatologies / 'out').mkdir()
     combine.combine(create_annual_per_variable_climatologies / 'in' / 'atmos', create_annual_per_variable_climatologies / 'out', 'atmos', 1980, 1981, 'yr', 'P2Y')
 
     output_dir = Path(create_annual_per_variable_climatologies, 'out', 'atmos', 'av', 'annual_2yr')
     output_file = output_dir / 'atmos.1980-1981.nc'
 
     assert output_file.exists()
+
+def test_combine_monthly_av(create_monthly_per_variable_climatologies):
+    """
+    Combine per-variable monthly climatologies into combined monthly climatology file
+    """
+
+    combine.combine(create_monthly_per_variable_climatologies / 'in' / 'atmos', create_monthly_per_variable_climatologies / 'out', 'atmos', 1980, 1981, 'mon', 'P2Y')
+
+    output_dir = Path(create_monthly_per_variable_climatologies, 'out', 'atmos', 'av', 'monthly_2yr')
+    for i in range(1,13):
+        output_file = output_dir / f'atmos.1980-1981.{i:02d}.nc'
+        assert output_file.exists()
