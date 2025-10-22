5341a8f871b6ac9b8b3895c8db9699831a01dba9
Ian Laflotte <ian.laflotte@noaa.gov>
2025-10-22 17:10:00 -0400

    break up mono-fixture into a few smaller ones and rewrite tests based on that

--- fre/app/mask_atmos_plevel/tests/test_mask_atmos_plevel.py
+++ fre/app/mask_atmos_plevel/tests/test_mask_atmos_plevel.py
@@ -13,57 +13,70 @@ import xarray as xr
 from fre.app.mask_atmos_plevel import mask_atmos_plevel
 
 @pytest.fixture()
-def create_input_files(tmp_path):
-    """
-    Create input data file atmos_cmip.ua_unmsk.nc and ps file atmos_cmip.ps.nc
-    in a temporary directory, and return the name of the temporary directory.
-    """
-    # path to input files
+def tmp_input(tmp_path):
+    # write netcdf files from the cdfs
+    # data (ua)
     input_ = Path('fre/tests/test_files/reduced_ascii_files/atmos_cmip.ua_unmsk.cdl')
-    ps     = Path('fre/tests/test_files/reduced_ascii_files/atmos_cmip.ps.cdl')
-    output = Path('fre/tests/test_files/reduced_ascii_files/atmos_cmip.ua_masked.cdl')
+    assert Path(input_).exists()
 
     # create temporary directory
     tmp_dir = tmp_path
     tmp_dir.mkdir(exist_ok=True)
-
-    # write netcdf files from the cdfs
-    # data (ua)
-    assert Path(input_).exists()
     tmp_input = Path(tmp_dir / "input.nc")
+
+    # ncgen
     command = [ 'ncgen', '-o', tmp_input, input_ ]
     sp = subprocess.run(command, check = True)
     assert sp.returncode == 0
     assert tmp_input.exists()
 
-    # ps
+    yield tmp_input
+
+@pytest.fixture()
+def tmp_ps(tmp_path):
+    # write netcdf files from the cdfs
+    # data (ps)
+    ps     = Path('fre/tests/test_files/reduced_ascii_files/atmos_cmip.ps.cdl')
     assert Path(ps).exists()
+
+    # create temporary directory
+    tmp_dir = tmp_path
+    tmp_dir.mkdir(exist_ok=True)
     tmp_ps = Path(tmp_dir / "ps.nc")
+
+    # ncgen
     command = [ 'ncgen', '-o', tmp_ps, ps ]
     sp = subprocess.run(command, check = True)
     assert sp.returncode == 0
     assert tmp_ps.exists()
 
-    # reference output (ua, masked)
-    assert Path(output).exists()
+    yield tmp_ps
+
+@pytest.fixture()
+def tmp_ref(tmp_path):
+    # write netcdf files from the cdfs
+    # data (ua)
+    ref = Path('fre/tests/test_files/reduced_ascii_files/atmos_cmip.ua_masked.cdl')
+    assert Path(ref).exists()
+
+    # create temporary directory
+    tmp_dir = tmp_path
+    tmp_dir.mkdir(exist_ok=True)
     tmp_ref = Path(tmp_dir / "ref.nc")
-    command = [ 'ncgen', '-o', tmp_ref, output ]
+
+    command = [ 'ncgen', '-o', tmp_ref, ref ]
     sp = subprocess.run(command, check = True)
     assert sp.returncode == 0
     assert tmp_ref.exists()
 
-    yield tmp_dir
+    yield tmp_ref
 
-
-def test_mask_atmos_plevel(create_input_files): # pylint: disable=redefined-outer-name
+def test_mask_atmos_plevel(tmp_input, tmp_ps, tmp_ref, tmp_path): # pylint: disable=redefined-outer-name
     """
     Do the pressure masking on the test input file,
     and then compare to a previously generated output file.
     """
-    tmp_input = Path(create_input_files / "input.nc")
-    tmp_ps = Path(create_input_files / "ps.nc")
-    tmp_output = Path(create_input_files / "output.nc")
-    tmp_ref = Path(create_input_files / "ref.nc")
+    tmp_output = Path(tmp_path / "output.nc")
 
     mask_atmos_plevel.mask_atmos_plevel_subtool(tmp_input, tmp_ps, tmp_output)
     assert tmp_output.exists()
@@ -73,15 +86,13 @@ def test_mask_atmos_plevel(create_input_files): # pylint: disable=redefined-oute
 
     assert ds['ua_unmsk'].values.all() == ds_ref['ua_unmsk'].values.all()
 
-def test_mask_atmos_plevel_recreate_output(create_input_files): # pylint: disable=redefined-outer-name
+
+def test_mask_atmos_plevel_recreate_output(tmp_input, tmp_ps, tmp_ref, tmp_path): # pylint: disable=redefined-outer-name
     """
     Do the pressure masking on the test input file, remaking an existing output file
     and then compare to a previously generated output file.
     """
-    tmp_input = Path(create_input_files / "input.nc")
-    tmp_ps = Path(create_input_files / "ps.nc")
-    tmp_output = Path(create_input_files / "output.nc")
-    tmp_ref = Path(create_input_files / "ref.nc")
+    tmp_output = Path(tmp_path / "output.nc")
 
     # copy the reference output to the output path, it should be re-created
     shutil.copy(str(tmp_ref), str(tmp_output))
@@ -94,14 +105,13 @@ def test_mask_atmos_plevel_recreate_output(create_input_files): # pylint: disabl
 
     assert ds['ua_unmsk'].values.all() == ds_ref['ua_unmsk'].values.all()
 
-def test_mask_atmos_plevel_nops_noop(create_input_files): # pylint: disable=redefined-outer-name
+
+def test_mask_atmos_plevel_nopsfile_noop(tmp_input, tmp_ps, tmp_path): # pylint: disable=redefined-outer-name
     """
     if the input file exists, but the ps file does not, then no-op gracefully
     without raising an exception or throwing an error
     """
-    tmp_input = Path(create_input_files / "input.nc")
-    tmp_ps = Path(create_input_files / "ps.nc")
-    tmp_output = Path(create_input_files / "output.nc")
+    tmp_output = Path(tmp_path / "output.nc")
 
     # remove the ps file
     tmp_ps.unlink()
@@ -111,6 +121,37 @@ def test_mask_atmos_plevel_nops_noop(create_input_files): # pylint: disable=rede
                                                 psfile = tmp_ps,
                                                 outfile = tmp_output)
     assert not tmp_output.exists()
+
+
+def test_mask_atmos_plevel_nops_error(tmp_input, tmp_path): # pylint: disable=redefined-outer-name
+    """
+    if the input and ps files exist, but the ps file does not have ps within it, raise ValueError
+    """
+    tmp_ps = tmp_input # this is OK, it just has to NOT have ps
+    tmp_output = Path(tmp_path / "output.nc")
+
+    with pytest.raises(ValueError):
+        mask_atmos_plevel.mask_atmos_plevel_subtool(infile = tmp_input,
+                                                    psfile = tmp_ps,
+                                                    outfile = tmp_output,
+                                                    warn_no_ps = False)
+    assert not tmp_output.exists()
+
+
+def test_mask_atmos_plevel_nops_warn(tmp_input, tmp_path): # pylint: disable=redefined-outer-name
+    """
+    if the input file exists, but the ps file does not, then no-op gracefully
+    without raising an exception or throwing an error while warn_no_ps = True
+    """
+    tmp_ps = tmp_input # this is OK, it just has to NOT have ps
+    tmp_output = Path(tmp_path / "output.nc")
+
+    mask_atmos_plevel.mask_atmos_plevel_subtool(infile = tmp_input,
+                                                psfile = tmp_ps,
+                                                outfile = tmp_output,
+                                                warn_no_ps = True)
+    assert not tmp_output.exists()
+
 
 def test_mask_atmos_plevel_exception():
     """ if the input file doesnt exist, error """
