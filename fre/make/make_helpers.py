ffb036f15354aadd8ffbb65541b5d5be43d01016
Dana Singh <Dana.Singh@noaa.gov>
2025-11-06 16:21:46 -0500

    #659 Fix spacing

--- fre/make/make_helpers.py
+++ fre/make/make_helpers.py
@@ -35,10 +35,10 @@ def get_mktemplate_path(mk_template: str, container_flag: bool, model_root: Opti
             topdir = Path(__file__).resolve().parents[1]
             template_path = str(topdir)+ "/mkmf/templates/"+mk_template
 
-        # Check in template path exists
-        if not Path(template_path).exists():
-            raise ValueError("Error w/ mkmf template. Created path from given "
-                             f"filename: {template_path} does not exist.")
+            # Check in template path exists
+            if not Path(template_path).exists():
+                raise ValueError("Error w/ mkmf template. Created path from given "
+                                 f"filename: {template_path} does not exist.")
     else:
         if "/" not in mk_template:
             template_path = model_root+"/mkmf/templates/"+mk_template
