6923876fc42b59ce2633116cb6dbd43a7b8b355e
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-01-27 15:42:14 -0500

    #697 Add fre workflow checkout tool and test

---
+++ fre/workflow/checkout_script.py
@@ -0,0 +1,120 @@
+""" Workflow checkout """
+import os
+import subprocess
+from pathlib import Path
+import logging
+
+import fre.yamltools.combine_yamls_script as cy
+from fre.app.helpers import change_directory
+#from . import make_workflow_name
+
+fre_logger = logging.getLogger(__name__)
+
+FRE_PP_WORKFLOW = "https://github.com/NOAA-GFDL/fre-workflows.git"
+FRE_RUN_WORKFLOW = "https://github.com/NOAA-GFDL/fre-workflows.git"
+
+def workflow_checkout(yamlfile: str = None, experiment = None,
+                      application = None, branch = None):
+    """
+    Create a directory and clone the workflow template files from a defined repo.
+
+    :param yamlfile: Model yaml configuration file
+    :type yamlfile: str
+    :param experiment: One of the postprocessing experiment names from the
+                       yaml displayed by fre list exps -y $yamlfile 
+                       (e.g. c96L65_am5f4b4r0_amip), default None
+    :type experiment: str
+    :param platform: The location + compiler that was used to run the model
+                     (e.g. gfdl.ncrc5-deploy), default None
+    :type platform: str
+    :param target: Options used for the model compiler (e.g. prod-openmp), default None
+    :type target: str
+    :param branch: which git branch to pull from, default None
+    :type branch: str
+    :param application: Which workflow will be used/cloned
+    :type application: str
+    :raises OSError: why checkout script was not able to be created
+    :raises ValueError:
+        -if experiment or platform or target is None
+        -if branch argument cannot be found as a branch or tag
+    """
+    # Used in consolidate_yamls function for now
+    platform = None
+    target = None
+    if application == "run":
+        fre_logger.info("NOT DONE YET")
+        # will probably be taken out and put above is "use"
+        # is generalized in this tool
+        yaml = cy.consolidate_yamls(yamlfile=yamlfile,
+                                    experiment=experiment,
+                                    platform=platform,
+                                    target=target,
+                                    use="run",
+                                    output=None)
+        workflow_info = yaml.get("workflow").get("run_workflow")
+    elif application == "pp":
+        # will probably be taken out and put above is "use"
+        # is generalized in this tool
+        yaml = cy.consolidate_yamls(yamlfile=yamlfile,
+                                    experiment=experiment,
+                                    platform=platform,
+                                    target=target,
+                                    use="pp",
+                                    output=None)
+        workflow_info = yaml.get("workflow").get("pp_workflow")
+
+    repo = workflow_info.get("repo")
+
+    if not branch:
+        tag = workflow_info.get("version")
+        fre_logger.info("Default tag ==> '%s'", tag)
+    else:
+        tag = branch
+        fre_logger.info("Requested branch/tag ==> '%s'", tag)
+
+    if None in [repo, tag]:
+        raise ValueError(f"One of these are None: repo / tag = {repo} / {tag}")
+
+    fre_logger.warning("(%s):(%s) check out ==> REQUESTED", repo, tag)
+
+    # clone directory
+    directory = os.path.expanduser("~/cylc-src")
+    # workflow name
+    workflow_name = experiment
+
+    # create workflow in cylc-src
+    try:
+        Path(directory).mkdir(parents=True, exist_ok=True)
+    except Exception as exc:
+        raise OSError(
+            f"(checkoutScript) directory {directory} wasn't able to be created. exit!") from exc
+
+    if not Path(f"{directory}/{workflow_name}").is_dir():
+        # scenarios 1+2, checkout doesn't exist, branch specified (or not)
+        fre_logger.info("Workflow does not yet exist; will create now")
+        clone_output = subprocess.run( ["git", "clone","--recursive",
+                                        f"--branch={tag}",
+                                        repo, f"{directory}/{workflow_name}"],
+                                        capture_output = True, text = True, check = True)
+        fre_logger.debug(clone_output)
+        fre_logger.warning("(%s):(%s) check out ==> SUCCESSFUL", repo, tag)
+    else:
+        # the repo checkout does exist, scenarios 3 and 4.
+        with change_directory(f"{directory}/{workflow_name}"):
+            # capture the branch and tag
+            # if either match git_clone_branch_arg, then success. otherwise, fail.
+            current_tag = subprocess.run(["git","describe","--tags"],
+                                         capture_output = True,
+                                         text = True, check = True).stdout.strip()
+            current_branch = subprocess.run(["git", "branch", "--show-current"],
+                                             capture_output = True,
+                                             text = True, check = True).stdout.strip()
+
+            if tag in (current_tag, current_branch):
+                fre_logger.warning("Checkout exists ('%s/%s'), and matches '%s'", directory, workflow_name, tag)
+            else:
+                fre_logger.error(
+                    "ERROR: Checkout exists ('%s/%s') and does not match '%s'", directory, workflow_name, tag)
+                fre_logger.error(
+                    "ERROR: Current branch: '%s', Current tag-describe: '%s'", current_branch, current_tag)
+                raise ValueError('Neither tag nor branch matches the git clone branch arg')
