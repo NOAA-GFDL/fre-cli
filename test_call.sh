ac2546f7a93f0edc647b8aa227c07d59994eaef3
Ian Laflotte <ilaflott@gmail.com>
2026-02-03 13:32:47 -0500

    add a cmip6 flavored call to the test_call script for my own convenience

--- test_call.sh
+++ test_call.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+
+## CMIP6 call here just in-case
+#fre -vv cmor run \
+#	--run_one \
+#	--indir fre/tests/test_files/ocean_sos_var_file \
+#	--varlist fre/tests/test_files/varlist \
+#	--table_config fre/tests/test_files/cmip6-cmor-tables/Tables/CMIP6_Omon.json \
+#	--exp_config fre/tests/test_files/CMOR_input_example.json \
+#	--outdir outdir \
+#	--calendar 'julian' \
+#	--grid_label 'gr' \
+#	--grid_desc 'foo bar placehold' \
+#	--nom_res '10000 km'
+##	--opt_var_name sos
+
+
+
+# CMIP7 call
 fre -vv cmor run \
 	--run_one \
 	--indir fre/tests/test_files/ocean_sos_var_file \
