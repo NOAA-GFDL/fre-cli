1ff19b1941aadebe857d8d2e9d6ab0871d1da7d9
Ian Laflotte <ilaflott@gmail.com>
2026-02-04 12:12:33 -0500

    remove scratch-work scripts i was using

--- setupenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-which conda || module load conda && echo "conda loaded"
-which fre || conda activate fre-cli && echo "fre loaded"
-which mkmf || export PATH=/home/inl/Working/fre-cli/fre/mkmf/bin:$PATH && echo "mkmf in path"
