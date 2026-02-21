9dc60325dba2f0dbac6f7762937e19b532ce8d91
Ian Laflotte <ilaflott@gmail.com>
2026-02-20 19:03:37 -0500

    after extensive testing

--- test_call.sh
+++ test_call.sh
@@ -5,28 +5,29 @@
 ## forms variable lists based on that and a table target
 ## writes ESM45xml/yaml_workflow/esm45-v14/test_cmor_yaml_writer_output.yaml
 #python quick_script.py
-fre -v -v cmor config \
-    --pp_dir /work/inl/CMIP7/ESM4/DECK/ESM4.5-picontrol/gfdl.ncrc6-intel25-prod-openmp/pp \
-    --mip_tables_dir /home/$USER/Working/fre-cli/fre/tests/test_files/cmip7-cmor-tables/tables \
-    --mip_era cmip7 \
-    --exp_config /home/inl/Working/fre-cli/fre/tests/test_files/CMOR_CMIP7_input_example.json \
-    --output_dir /net2/$USER/Working/fre-cli/ESM45xml/test_cmor_output \
-    --varlist_dir /home/$USER/Working/fre-cli/test_variable_lists \
-    --freq monthly \
-    --chunk 5yr \
-    --grid g99 \
-    --overwrite \
-    --calendar noleap \
-    --output_yaml /home/inl/Working/ESM45xml/yaml_workflow/esm45-v14/test_cmor_yaml_writer_prototype.yaml
+#fre -v cmor config \
+#fre -v -v cmor config \
+#    --pp_dir /work/inl/CMIP7/ESM4/DECK/ESM4.5-picontrol/gfdl.ncrc6-intel25-prod-openmp/pp \
+#    --mip_tables_dir /home/$USER/Working/fre-cli/fre/tests/test_files/cmip7-cmor-tables/tables \
+#    --mip_era cmip7 \
+#    --exp_config /home/inl/Working/fre-cli/fre/tests/test_files/CMOR_CMIP7_input_example.json \
+#    --output_dir /net2/$USER/Working/fre-cli/ESM45xml/test_cmor_output \
+#    --varlist_dir /home/$USER/Working/fre-cli/test_variable_lists \
+#    --freq monthly \
+#    --chunk 5yr \
+#    --grid g99 \
+#    --overwrite \
+#    --calendar noleap \
+#    --output_yaml /home/inl/Working/ESM45xml/yaml_workflow/esm45-v14/test_cmor_yaml_writer_prototype.yaml
 
 # some calls from yaml development for ESM45xml, can generate them with:
 fre -v -v cmor yaml \
     -e ESM4.5-picontrol \
     -p gfdl.ncrc6-intel25 \
     -t prod-openmp \
-    -y /home/inl/Working/ESM45xml/yaml_workflow/esm45-v14/ESM4.5v14.yaml \
-    --run_one \
-    --dry_run
+    -y /home/inl/Working/ESM45xml/yaml_workflow/esm45-v14/ESM4.5v14.yaml #\
+#    --run_one #\
+#    --dry_run
 
 
 ## ocean_monthly / ts
