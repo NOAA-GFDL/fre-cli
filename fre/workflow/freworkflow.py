15c0802197359b3c35316e259cca4416329313ad
Dana Singh <Dana.Singh@an210.princeton.rdhpcs.noaa.gov>
2026-02-26 16:30:53 -0500

    #698 Update install script

--- fre/workflow/freworkflow.py
+++ fre/workflow/freworkflow.py
@@ -1,5 +1,5 @@
 ''' fre workflow '''
-
+import os
 import click
 import logging
 fre_logger = logging.getLogger(__name__)
@@ -13,89 +13,27 @@ from . import install_script
 def workflow_cli():
     ''' entry point to fre workflow click commands '''
 
-#@workflow_cli.command()
-#@click.option("-y", "--yamlfile", type=str,
-#              help="Model yaml file",
-#              required=True)
-#@click.option("-e", "--experiment", type=str,
-#              help="Experiment name",
-#              required=True)
-##@click.option("-p", "--platform", type=str,
-##              help="Platform name")
-##@click.option("-t", "--target", type=str,
-##              help="Target name")
-#@click.option("-b", "--branch", type =str,
-#              required=False, default = None,
-#              help="fre-workflows branch/tag to clone; default is $(fre --version)")
-#@click.option("-a", "--application",
-#              type=click.Choice(['run', 'pp']),
-#              help="Use case for checked out workflow",
-#              required=True)
-#def checkout(yamlfile, experiment, application, branch=None):
-#    """
-#    Checkout/extract fre workflow
-#    """
-#    checkout_script.workflow_checkout(yamlfile, experiment, application, branch)
-
 @workflow_cli.command()
-@click.option("-e", "--experiment", type=str,
-              help="Experiment name",
-              required=True)
-#@click.option("-p", "--platform", type=str,
-#              help="Platform name",
-#              required=True)
-#@click.option("-t", "--target", type=str,
-#              help="Target name",
-#              required=True)
-def install(experiment):
+@click.option("-e", "--experiment",
+              type=str,
+              required=True,
+              help="Experiment name")
+@click.option("--src-dir",
+              type=str,
+              envvar="TMPDIR",
+              default=os.path.expanduser("~/.fre"),
+              required=True,
+              help="Path to cylc-src directory")
+@click.option("--target-dir",
+              type=str,
+              help="""Target directory to install the cylc
+                      workflow into. Default location is
+                      ~/cylc-run/<workflow name>""")
+@click.option("--force-install",
+              type=bool,
+              help="If cylc-run/[workflow_name] exists")
+def install(experiment, src_dir, target_dir):
     """
     Install workflow configuration
     """
-    install_script.workflow_install(experiment)
-
-#@workflow_cli.command()
-#@click.option("-e", "--experiment", type=str,
-#              help="Experiment name",
-#              required=True)
-##@click.option("-p", "--platform", type=str,
-##              help="Platform name",
-##              required=True)
-##@click.option("-t", "--target", type=str,
-##              help="Target name",
-##              required=True)
-#@click.option("--pause", is_flag=True, default=False,
-#              help="Pause the workflow immediately on start up",
-#              required=False)
-#@click.option("--no_wait", is_flag=True, default=False,
-#              help="after submission, do not wait to ping the scheduler and confirm success",
-#              required=False)
-#def run(experiment, pause, no_wait):
-#    """
-#    Run workflow configuration
-#    """
-#    run_script.workflow_run(experiment, pause, no_wait)
-#
-#@workflow_cli.command()
-#@click.option("-e", "--experiment", type=str,
-#              help="Experiment name",
-#              required=True)
-##@click.option("-p", "--platform", type=str,
-##              help="Platform name",
-##              required=True)
-##@click.option("-T", "--target", type=str,
-##              help="Target name",
-##              required=True)
-#@click.option("-c", "--config-file", type=str,
-#              help="Path to a configuration file in either XML or YAML",
-#              required=True)
-#@click.option("-b", "--branch",
-#              required=False, default=None,
-#              help="fre-workflows branch/tag to clone; default is $(fre --version)")
-#@click.option("-t", "--time",
-#              required=False, default=None,
-#              help="Time whose history files are ready")
-#def all(experiment, platform, target, config_file, branch, time):
-#    """
-#    Execute all fre workflow initialization steps in order
-#    """
-#    wrapper_script.run_workflow_steps(experiment, platform, target, config_file, branch, time)
+    install_script.workflow_install(experiment, src_dir, target_dir)
--- fre/workflow/install_script.py
+++ fre/workflow/install_script.py
@@ -1,49 +1,72 @@
 ''' fre workflow install '''
-
 from pathlib import Path
-import os
 import subprocess
 import logging
-fre_logger =logging.getLogger(__name__)
-
-from . import make_workflow_name
 from fre.app.helpers import change_directory
 
-def workflow_install(experiment):
+fre_logger =logging.getLogger(__name__)
+
+def workflow_install(experiment: str, src_dir: str, target_dir: str, force_install: bool):
     """
     Install the Cylc workflow definition located in
+    cylc-src/$(experiment) to cylc-run/$(experiment)
 
-    ~/cylc-src/$(experiment)
-
-    to
-
-    ~/cylc-run/$(experiment)
-
-    :param experiment: Experiment names from the yaml displayed by fre list exps -y $yamlfile (e.g. c96L65_am5f4b4r0_amip), default None
+    :param experiment: Experiment names from the yaml displayed by
+                       fre list exps -y $yamlfile (e.g. c96L65_am5f4b4r0_amip);
+                       default None
     :type experiment: str
+    :param src_dir: src_dir/workflow_name
+    :type src_dir:
+    :param target_dir:
+    :type target_dir:
     """
-
-    #name = experiment + '__' + platform + '__' + target
-#    workflow_name = make_workflow_name(experiment, platform, target)
     workflow_name = experiment
+
+    # Check src_dir exists
+    if not Path(src_dir):
+        raise ValueError("""Cylc source directory ({src_dir}) could not be found! Try specifying
+                            path by passing --src-dir [path].""")
 
     # if the cylc-run directory already exists,
     # then check whether the cylc expanded definition (cylc config)
     # is identical. If the same, good. If not, bad.
-    source_dir = Path(os.path.expanduser("~/cylc-src"), workflow_name)
-    install_dir = Path(os.path.expanduser("~/cylc-run"), workflow_name)
-    if os.path.isdir(install_dir):
-        with change_directory(source_dir):
+    install_dir = Path(f"{target_dir}/cylc-run")
+    if Path(install_dir).is_dir():
+        fre_logger.warning(" *** PREVIOUS INSTALL FOUND: %s ***", install_dir)
+        if force_install:
+            fre_logger.warning(" *** REMOVING %s *** ", install_dir)
+            install_output = subprocess.run(["cylc", "clean", f"{install_dir}/{workflow_name}"],
+                                            capture_output = True,
+                                            text = True,
+                                            check = True)
+            fre_logger.debug(install_output)
+        else:
             # must convert from bytes to string for proper comparison
-            installed_def = subprocess.run(["cylc", "config", workflow_name],capture_output=True).stdout.decode('utf-8')
-            source_def = subprocess.run(['cylc', 'config', '.'], capture_output=True).stdout.decode('utf-8')
+            installed_def = subprocess.run(["cylc", "config", workflow_name],
+                                           capture_output=True,
+                                           check=True).stdout.decode('utf-8')
+            with change_directory(src_dir):
+                source_def = subprocess.run(['cylc', 'config', '.'],
+                                            capture_output=True,
+                                            check=True).stdout.decode('utf-8')
+
             if installed_def == source_def:
-                fre_logger.warning(f"NOTE: Workflow '{install_dir}' already installed, and the definition is unchanged")
+                fre_logger.warning("""NOTE: Workflow '%s/%s}' already ",
+                                      installed, and the definition is unchanged""", install_dir, workflow_name)
             else:
-                fre_logger.error(f"ERROR: Please remove installed workflow with 'cylc clean {workflow_name}'"
-                      " or move the workflow run directory '{install_dir}'")
-                raise Exception(f"ERROR: Workflow '{install_dir}' already installed, and the definition has changed!")
-    else:
-        fre_logger.warning(f"NOTE: About to install workflow into ~/cylc-run/{workflow_name}")
-        cmd = f"cylc install --no-run-name {workflow_name}"
-        subprocess.run(cmd, shell=True, check=True)
+                fre_logger.error("ERROR: Please remove installed workflow with one of these options:")
+                fre_logger.error("  - fre workflow install -e %s --src-dir %s --target-dir %s --force-install", experiment, src_dir, target_dir)
+                fre_logger.error("  - cylc clean %s/%s, then re-run install command", install_dir, workflow_name)
+                raise ValueError(f"""ERROR: Workflow '{install_dir}/{workflow_name}' already
+                                     installed, and the definition has changed!""")
+
+    if not Path(install_dir).is_dir():
+        fre_logger.warning("NOTE: About to install workflow into ~/cylc-run/%s", workflow_name)
+        if not target_dir:
+            # install workflow in default home location
+            cmd = f"cylc install --no-run-name {src_dir}"
+            subprocess.run(cmd, shell=True, check=True)
+        else:
+            # symlink the workflow and associated files in target_dir
+            cmd = f"cylc install --no-run-name {src_dir} --symlink-dirs='{target_dir}/{workflow_name}'"
+            subprocess.run(cmd, shell=True, check=True)
