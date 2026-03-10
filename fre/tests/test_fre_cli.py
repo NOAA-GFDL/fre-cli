07d491f9f535bb6c275e0ea9f1cc60bb78f24357
Dana Singh <Dana.Singh@an106.princeton.rdhpcs.noaa.gov>
2026-03-10 16:13:10 -0400

    adds test for when testing tag is pushed
    - checks for correct testing tag as well as previous tests for checking full fre version

--- fre/tests/test_fre_cli.py
+++ fre/tests/test_fre_cli.py
@@ -10,7 +10,7 @@ We've got 4-ish tests:
   right and thinks the tool has a --optionDNE option)
 - fre --version, checking for version GTE current version (fails if version isn't defined)
 """
-
+import subprocess
 from click.testing import CliRunner
 
 from fre import fre
@@ -35,11 +35,18 @@ def test_cli_fre_option_dne():
 
 def test_fre_version():
     ''' module import flavor of below cli test '''
-    assert '2026.01-alpha1' == fre.version
+    assert '2025.04' == fre.version
 
 def test_cli_fre_version():
     ''' fre --version '''
     result = runner.invoke(fre.fre, args='--version')
-    expected_out = 'fre, version 2026.01-alpha1'
+    expected_out = 'fre, version 2025.04'
     assert all( [ result.exit_code == 0,
                   expected_out in result.output ] )
+
+def test_fre_version_testing_tag():
+    ''' module import flavor of below cli test '''
+    result = subprocess.run(["git", "tag", "--list", "--sort=-creatordate"], text=True, check=True, capture_output=True)
+    latest_testing_tag = result.stdout.split('\n')[0]
+
+    assert '2026.01-alpha1' == latest_testing_tag
