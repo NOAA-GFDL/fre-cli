156a1d1a14af49ac35c5c69a752a5d862130bc26
Ian Laflotte <ian.laflotte@noaa.gov>
2026-04-07 11:57:25 -0400

    add script tracking which branches we have merged in, to QA the pieces together as a prerelease for the frecmor submodule

---
+++ to_merge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#git merge origin/copilot/remove-replace-nccmp-from-tests
+#git merge origin/copilot/remove-replace-nccmp-test-remap-pp-components
+#git merge origin/copilot/remove-replace-nccmp-in-tests
+#git merge origin/copilot/deprecate-cdo-python-cdo-use
+#git merge origin/copilot/add-empty-user-experiment-config
+#git merge origin/copilot/fix-cmor-error-cmip6-cmip7
+#git merge origin/copilot/fix-flexibility-calendar-input
+git merge fork/frecmor-updates-for-cmip7
