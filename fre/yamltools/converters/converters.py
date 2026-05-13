e17a30b12bdf5e340d73355f2e9122ef57117ba2
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2026-05-13 09:27:47 -0400

    update

--- fre/yamltools/converters/converters.py
+++ fre/yamltools/converters/converters.py
@@ -1,78 +1,130 @@
 from pathlib import Path
 import yaml
 
-from bs4 import BeautifulSoup 
-
-from xml import XML
+try:
+    from .xml import XML
+except ImportError:
+    from xml import XML
 
 
 class CompileConverter(XML):
+    """
+    Converter class to parse compile experiment XML blocks and convert to YAML dictionaries.
+    """
     
     def __init__(self, xmlfile: str|Path, experiment_name: str = None):
 
         super().__init__(xmlfile)
+        self.experiments = self.get_experiments(experiment_name)
+        self.yaml_dicts = {}
 
-        self.experiment_name = experiment_name
-        self.experiments = self.set_experiments(experiment_name)
-        self.yamldicts = {}
 
+    def get_experiments(self, experiment_name: str = None):
+        """
+        Parse the XML and save each compile experiment block 
+        """
+
+        #prettify name
+        prettify = lambda name: name.strip().lower().replace("$(", "").replace(")", "")
+
+        # if a particular experiment is not provided, get all compile experiments in xml    
+        experiments = self.get_elements("experiment", self.soup, name=experiment_name)
         
-    def convert(self, experiment_name: str = None):
-        
-        for experiment_name, experiment in self.experiments.items():                    
-            compile_yaml = {"compile": {
-                "experiment": experiment_name,
-                "container_addlibs": "",
-                "baremetal_linkerflags": "",
-                "src": self.get_src_list(experiment)}
-            }
-            self.yamldicts[experiment_name] = compile_yaml        
-
-            self.write_yaml(compile_yaml, "compile_"+experiment_name +".yaml")
-            
-        
-    def write_yaml(self, yamldict: dict, yamlfile: str|Path):    
-
-        with open(yamlfile, "w") as openedfile:
-            yaml.dump(yamldict, openedfile, sort_keys=False)
-
-
-    def set_experiments(self, experiment_name: str = None):
-
-        prettyname = lambda name: name.replace("$(", "").replace(")", "")
-        
-        experiments = self.find_all("experiment", search_name=experiment_name)
-
         if experiments:
             return {
-                prettyname(self.get_key("name", experiment)): experiment for experiment in experiments
+                prettify(self.get_attributes("name", experiment)): experiment for experiment in experiments
             }
 
         raise RuntimeError("Cannot find experiments")
 
-            
-    def get_src_list(self, experiment, component_name: str = None):
 
-        components = self.find_all("component", experiment, component_name)
+    def convert(self):        
+        """
+        Convert XML content to YAML dictionary
+        """
         
-        src_list = []
+        for experiment_name, experiment_content in self.experiments.items():                    
+            compile_yaml = {"compile": {
+                "experiment": experiment_name,
+                "container_addlibs": "",
+                "baremetal_linkerflags": "",
+                "src": self.parse_components(experiment_content)}
+            }
+            self.yaml_dicts[experiment_name] = compile_yaml        
+            self.write_yaml(compile_yaml, "compile_"+experiment_name +".yaml")
+                
+            
+    def parse_components(self, xml_content, component_name: str = None):
+        """
+        Parses component blocks to yaml dictionaries.  For example, converts 
+       
+        <component name="atmos_cubed_sphere" 
+                   paths="GFDL_atmos_cubed_sphere/model, GFDL_atmos_cubed_sphere/driver/SHiELD/cloud_diagnosis.F90", 
+                   requires="fms am5_phys">
+          <source versionControl="git" root="https://github.com/NOAA-GFDL">
+            <codeBase version="2024.01_am5">GFDL_atmos_cubed_sphere.git</codeBase>
+          </source>
+          <compile>
+            <makeOverrides>USE_R4=$(USE_MIXED_MODE) ISA="-march=core-avx2 -qno-opt-dynamic-align"</makeOverrides>
+            <cppDefs>$(F2003_FLAGS) -DSPMD -DCLIMATE_NUDGE</cppDefs>
+          </compile>
+        </component>
+       
+        to 
+       
+        {
+        component: atmos_cubed_sphere
+        repo: https://github.com/NOAA-GFDL/GFDL_atmos_cubed_sphere.git
+        branch: 2024.01_am5
+        paths: [GFDL_atmos_cubed_sphere/model, GFDL_atmos_cubed_sphere/driver/SHiELD/cloud_diagnosis.F90]
+        requires: [fms, am5_phys]
+        otherFlags: [$(F2003_FLAGS) -DSPMD -DCLIMATE_NUDGE]
+        makeOverrides : [USE_R4=$(USE_MIXED_MODE), ISA="-march=core-avx2, -qno-opt-dynamic-align"]
+        }
+        """
+
+        components = self.get_elements("component", xml_content, name=component_name)
+        if components is None:
+            return None
+            
+        parsed_components = []
         for component in components:
 
+            codebase = self.get_elements("codeBase", component)
+            source = self.get_elements("source", component)
+            cppdefs = self.get_elements("cppDefs", component)
+            compile_ = self.get_elements("compile", component)
+            make_overrides = self.get_elements("makeOverrides", component)
+            csh = self.get_elements("csh", component)
+
             component_yaml = {
-                "component": self.get_key("name", component),
-                "branch": self.get_tag("codeBase", component, "version"),
-                "repo": self.get_tag("source", component, "root") + "/" + self.get_tag("codeBase", component),
-                "paths": self.make_list(self.get_key("paths", component)),
-                "requires": self.make_list(self.get_key("requires", component)),
-                "otherFlags": self.make_list(self.get_key("includeDir", component)),
-                "cppdefs": self.get_tag("cppDefs", component),
-                "makeOverrides": self.get_tag("makeOverrides", component),
-                "doF90Cpp": self.get_tag("compile", component, "doF90Cpp"),
-                "additionalInstructions": self.make_list(self.get_tag("csh", component), "\n")
+                "component": self.get_attributes("name", component),
+                "repo": f"{self.get_attributes('root', source)}/{self.get_values(codebase)}",
+                "branch": self.get_attributes("version", codebase),
+                "paths": self.get_attributes("paths", component, tolist=True),
+                "requires": self.get_attributes("requires", component, tolist=True),
+                "otherFlags": self.get_attributes("includeDir", component, tolist=True),
+                "cppdefs": self.get_values(cppdefs),
+                "makeOverrides": self.get_values(make_overrides),
+                "doF90Cpp": self.get_attributes("doF90Cpp", compile_),
+                "additionalInstructions": self.get_values(csh, tolist=True, fieldsep="\n")
             }
             
-            src_list.append(
-                {key: value for key, value in component_yaml.items() if value is not None}
-            )
+            #remove None
+            component_yaml = {key: value for key, value in component_yaml.items() if value is not None}
+            parsed_components.append(component_yaml)
             
-        return src_list
+        return parsed_components
+
+
+    def write_yaml(self, yamldict: dict, yamlfile: str|Path):           
+        """
+        Write YAML dictionary to file
+        """
+
+        with open(yamlfile, "w", encoding="utf-8") as openedfile:
+            yaml.dump(yamldict, openedfile, sort_keys=False)
+
+
+xml = CompileConverter("compile_experiment.xml")
+xml.convert()
--- fre/yamltools/converters/xml.py
+++ fre/yamltools/converters/xml.py
@@ -1,84 +1,81 @@
 from pathlib import Path
 
-from bs4 import BeautifulSoup 
-import yaml
+from bs4 import BeautifulSoup
 
 
-class XML():
-    
-    def __init__(self, xmlfile: str|Path):
+class XML:
+    """
+    Base class to parse XML files and extract elements and key values using BeautifulSoup.
+    """
+
+    def __init__(self, xmlfile: str | Path):
 
         self.xmlfile = Path(xmlfile)
-
         if not self.xmlfile.exists():
             raise IOError(f"Cannot find XML file {xmlfile}")
-        
-        with open(xmlfile, "r") as openedfile:
-            self.soup = BeautifulSoup(openedfile, "lxml-xml")
+
+        with open(xmlfile, "r", encoding="utf-8") as openedfile:
+            xml_content = openedfile.read()
+
+        self.soup = BeautifulSoup(xml_content, "lxml-xml")
 
 
-    def get_key(self, tagid: str, tagobj= None):
+    def get_attributes(self, attribute: str, element: dict, tolist: bool = False, fieldsep: str = ","):
+        """
+        Get attribute value from an XML element.  For example, 
 
-        if tagobj is None:
-            tagobj = self.soup
-        
-        tagid = tagobj.get(tagid)
+        element = <source versionControl="git" root="http://github.com/NOAA-GFDL">
+        get_attributes("root", element) returns "http://github.com/NOAA-GFDL"
 
-        if tagid is not None:
-            tagid = tagid.strip()
-            if tagid:
-                return tagid
+        element = <component name="am5_phys" requires="fms rte-rrtmgp rte-ecckd">
+        get_attributes("requires", element, tolist=True) returns ["fms", "rte-rrtmgp", "rte-ecckd"]
+        """
 
-        return None
+        value = element.get(attribute)
 
-
-    def find_all(self, tag: str, soup = None, search_name: str = None):
-
-        if soup is None:
-            soup = self.soup
-
-        args = [tag]
-        if search_name is not None:
-            args.append({"name": search_name})
-
-        tagobjs = soup.find_all(*args)
-        
-        return tagobjs
-        
-
-    def get_tag(self, soup = None):
-
-        if soup is None:
-            soup = self.soup    
-    
-        tagobjs = soup.find_all(tags)
-
-        if not tagobjs:
-            return None
-
-        return tagobjs
-               
-        
-    def get_tag(self, tag, soup = None, tagid: str = None):
-
-        tagobj = soup.find(tag)
-
-        if tagobj is None:
-            return None
-        
-        if tagid is not None:
-            return self.get_key(tagobj, tagid)
-        
-        tagstr = tagobj.text.strip()
-        if tagstr:
-            return tagstr
+        if value is not None:
+            value = value.strip()
+            if value:
+                if tolist:
+                    return [val.strip() for val in value.split(fieldsep)]
+                return value
 
         return None
 
-    
-    def make_list(self, tag_value: str, split_token: str = None):
 
-        if tag_value is None:
-            return None
-        
-        return [val.strip() for val in tag_value.split(split_token)]
+    def get_elements(self, element: str, xml_content, name: str = None, find_all: bool = True):
+        """
+        Get XML elements by tag name and optional attribute value.  For example,
+
+        get_elements("component", soup) returns all <component> blocks in the XML.
+        get_elements("component", soup, name="am5_phys") returns the <component> element block with name="am5_phys"
+        """ 
+
+        search_dict = {}
+        if name is not None:
+            search_dict["name"] = name
+
+        find = xml_content.find_all if find_all else xml_content.find
+        element = find(element, search_dict)
+
+        return element
+
+
+    def get_values(self, element, tolist: bool = False, fieldsep: str = ","):
+        """
+        Get value for an XML element.  For example, 
+
+        element = <cppDefs> -DDEBUG -Iinclude</cppDefs>, 
+        get_values(element, tolist=True) returns ["-DDEBUG", "-Iinclude"]
+        """
+
+        value = element.text.strip()
+
+        if value:
+            if tolist:
+                if fieldsep is None:
+                    return value.split()
+                return [val.strip() for val in value.split(fieldsep)]
+            return value
+
+        return None
