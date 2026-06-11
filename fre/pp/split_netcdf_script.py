c642ec844868f48f8895ed1f0f19ea62d838583e
Chris Blanton <chris.blanton@noaa.gov>
2026-06-11 14:01:30 -0400

    #908 Fix mkdir race condition by using exist_ok=True

--- fre/pp/split_netcdf_script.py
+++ fre/pp/split_netcdf_script.py
@@ -129,8 +129,7 @@ def split_netcdf(
                 fre_logger.info(f"No input files found; skipping subdir {sd}")
             else:
                 output_subdir = os.path.join(os.path.abspath(outputDir), sd)
-                if not os.path.isdir(output_subdir):
-                    os.mkdir(output_subdir)
+                os.mkdir(output_subdir, exist_ok=True)
                 for infile in files:
                     split_file_xarray(infile, output_subdir, varlist)
                     files_split += 1
