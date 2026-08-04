3e1afe9a476707b2492352ee5c5cffc70f0f0248
Ciheim <43519680+Ciheim@users.noreply.github.com>
2026-08-04 13:22:58 -0400

    Update variable_id in catalog specification description

--- fre/catalog/frecatalog.py
+++ fre/catalog/frecatalog.py
@@ -7,7 +7,7 @@ query, and load data consistently.
 The cataloging ecosystem is composed of three main components:
 
 1. Catalog Specification (JSON): A single file that provides metadata about 
-   the catalog (instituion_id, experiment, file paths, etc.). It supports extensible metadata and identifies how multiple distinct 
+   the catalog (variable_id, experiment, file paths, etc.). It supports extensible metadata and identifies how multiple distinct 
    files can be aggregated into a single cohesive "dataset."
 2. Catalog (CSV): A file that acts as the index file for the data collection, providing 
    the paths to the data files and their associated metadata at a user-defined granularity.
