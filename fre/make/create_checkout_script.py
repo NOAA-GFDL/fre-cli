a457ef7c07ab19c284e8ec6b5fbaf4b883724195
Dana Singh <Dana.Singh@ldt-4742397.gfdl.noaa.gov>
2026-08-18 17:19:25 -0400

    actually address some pylint errors ha

--- fre/make/create_checkout_script.py
+++ fre/make/create_checkout_script.py
@@ -7,8 +7,6 @@ The method checkout_create is the entry point called by fre make checkout-script
 fre make all.  Checkout_create calls baremetal_checkout_write for a bare-metal build or 
 container_checkout_write for a container build to write checkout.sh.
 """
-import sys
-import shutil
 from pathlib import Path
 from datetime import datetime
 import subprocess
@@ -164,7 +162,7 @@ def checkout_create(yamlfile: str, platform: tuple, target: tuple,
 
     # Validate the targets
     for t in target:
-        valid_t = targetfre.fretarget(t)
+        targetfre.fretarget(t)
 
     fre_logger.setLevel(level=logging.INFO)
 
